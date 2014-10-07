191ae084b7fe3d7f85af65b33e83b1093dd547c0
Sebastian Marcet <smarcet@gmail.com>
2014-10-07 10:50:44 -0500

    [smarcet] - #7541 * fixed errors on Candidate
    Conflicts:
    
    	openstack/code/Candidate.php

--- openstack/code/Candidate.php
+++ openstack/code/Candidate.php
@@ -30,13 +30,17 @@ class Candidate extends DataObject
 	{
 		// Load the election system
 		$Elections = ElectionSystem::get()->first();
+		if(!$Elections) return false;
 		return $Elections->CurrentElection();
 	}
 
 	// Return the number of nominations for this candidate
 	function countNominations()
 	{
-		$Nominations = $this->CurrentElection()->CandidateNominations("`CandidateID` = " . $this->MemberID);
+		$current_election = $this->CurrentElection();
+		if(!$current_election) return 0;
+
+		$Nominations = $current_election->CandidateNominations("`CandidateID` = " . $this->MemberID);
 		return $Nominations->Count();
 	}
 
@@ -44,28 +48,36 @@ class Candidate extends DataObject
 	function hasNominated()
 	{
 		if ($LoggedInMemberID = Member::currentUserID()) {
-			return $this->CurrentElection()->CandidateNominations("`CandidateID` = " . $this->MemberID . " AND `MemberID` = " . $LoggedInMemberID);
+			$current_election = $this->CurrentElection();
+			if(!$current_election) return false;
+			return $current_election->CandidateNominations("`CandidateID` = " . $this->MemberID . " AND `MemberID` = " . $LoggedInMemberID);
 		}
 	}
 
 	// Return if this candidate has received more than 10 nominations
 	function MoreThanTen()
 	{
-		$Nominations = $this->CurrentElection()->CandidateNominations("`CandidateID` = " . $this->MemberID);
+		$current_election = $this->CurrentElection();
+		if(!$current_election) return false;
+		$Nominations = $current_election->CandidateNominations("`CandidateID` = " . $this->MemberID);
 		return ($Nominations->Count() >= 10);
 	}
 
 	// Return if this candidate has received more than 10 nominations
 	function LessThanTen()
 	{
-		$Nominations = $this->CurrentElection()->CandidateNominations("`CandidateID` = " . $this->MemberID);
+		$current_election = $this->CurrentElection();
+		if(!$current_election) return false;
+		$Nominations = $current_election->CandidateNominations("`CandidateID` = " . $this->MemberID);
 		return ($Nominations->Count() < 10);
 	}
 
 	// Return the nominations for this candidate
 	function getNominations()
 	{
-		return $this->CurrentElection()->CandidateNominations("`CandidateID` = " . $this->MemberID);
+		$current_election = $this->CurrentElection();
+		if(!$current_election) return false;
+		return $current_election->CandidateNominations("`CandidateID` = " . $this->MemberID);
 	}
 
 }
