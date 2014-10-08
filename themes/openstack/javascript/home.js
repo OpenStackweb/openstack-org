b1ad2c37e6916399a6e1f024bead83c2a6e62594
Jimmy McArthur <jimmy@tipit.net>
2014-10-08 17:20:56 +0100

    NEW DESIGN: Updated copy for homepage rollover.

--- themes/openstack/javascript/home.js
+++ themes/openstack/javascript/home.js
@@ -4,42 +4,42 @@ $('.hero-credit').tooltip()
 // Customer Stories, this should be improved
 $(function() {
     $("#bloomberg-logo").hover(function() {
-        $(".change-description").text("Bloomberg uses OpenStack for some really cool things.");
+        $(".change-description").text("The world relies on Bloomberg for billions of financial data points per day.");
         $(".customer-logos.logo-hover").removeClass("logo-hover");
         $(this).toggleClass("logo-hover");
     });
 });
 $(function() {
     $("#bestbuy-logo").hover(function() {
-        $(".change-description").text("BestBuy is pretty awesome and uses OpenStack in their stores.");
+        $(".change-description").text("Development teams at BestBuy rely on OpenStack to continuously deploy new features.");
         $(".customer-logos.logo-hover").removeClass("logo-hover");
         $(this).toggleClass("logo-hover");
     });
 });
 $(function() {
     $("#sony-logo").hover(function() {
-        $(".change-description").text("Sony's PS4 online network is run by OpenStack, allowing thousands to connect.");
+        $(".change-description").text("Sony relies on Openstack to deliver connected gaming experiences to millions of gamers.");
         $(".customer-logos.logo-hover").removeClass("logo-hover");
         $(this).toggleClass("logo-hover");
     });
 });
 $(function() {
     $("#comcast-logo").hover(function() {
-        $(".change-description").text("Comcast is using OpenStack to provide real-time programming guides and fast program search.");
+        $(".change-description").text("Comcast delivers interactive entertainment to millions of living rooms.");
         $(".customer-logos.logo-hover").removeClass("logo-hover");
         $(this).toggleClass("logo-hover");
     });
 });
 $(function() {
     $("#paypal-logo").hover(function() {
-        $(".change-description").text("PayPal uses OpenStack to run thousands of racks and so many other things too.");
+        $(".change-description").text("PayPal delivers features faster with their OpenStack private cloud.");
         $(".customer-logos.logo-hover").removeClass("logo-hover");
         $(this).toggleClass("logo-hover");
     });
 });
 $(function() {
     $("#wells-logo").hover(function() {
-        $(".change-description").text("Wells Fargo built online versions of heaven with all of the clouds they connected with OpenStack");
+        $(".change-description").text("The world’s most valuable bank relies on OpenStack.");
         $(".customer-logos.logo-hover").removeClass("logo-hover");
         $(this).toggleClass("logo-hover");
     });
