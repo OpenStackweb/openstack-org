dd65c745c0eb3fda8b37bedd30d29f5a1c7fec82
Sebastian Marcet <smarcet@gmail.com>
2014-10-14 12:26:29 -0500

    [smarcet] - #7573 * added wp db backup procedure

--- backup_scripts/cron-backup.php
+++ backup_scripts/cron-backup.php
@@ -13,9 +13,10 @@ require_once('cron-backup-config.php');
 set_time_limit(0);
 // Set the date and name for the backup files
 date_default_timezone_set('America/Chicago');
-$date            = date("M-d-Y_H-i-s");
-$backupname_site = "$url-backup-site-$date.zip";
-$backupname_db   = "$url-backup-db-$date.zip";
+$date             = date("M-d-Y_H-i-s");
+$backupname_site  = "$url-backup-site-$date.zip";
+$backupname_db    = "$url-backup-db-$date.zip";
+$backupname_db_wp = "$url-backup-db-wp-$date.zip";
 
 // Check for newer versions of script
 function file_get_data($url)
@@ -66,6 +67,25 @@ if ($db_backup == "true") {
 		shell_exec("zip -9pr $backupname_db db_backup.sql");
 		echo date("h:i:s") . " -- Database dump complete!\n";
 	}
+
+	// WP DB Backup
+	$db_connection2 = mysql_connect($db_wp_host, $db_wp_user, $db_wp_password);
+
+	if (!$db_connection2) {
+		echo date("h:i:s") . " -- WP Database connection failed! Check your database credentials in the cron-backup-config.php file.\n";
+		die();
+	} else {
+// Dump the mysql database
+		echo date("h:i:s") . " -- Starting WP database dump...\n";
+// First we ensure we are in the root of the site and not the root of account. This is needed since cron runs from the root of the account
+		chdir("$path");
+		chdir("../../");
+		shell_exec("mysqldump -h $db_wp_host -u $db_wp_user --password='$db_wp_password' $db_wp_name > db_wp_backup.sql");
+
+		echo date("h:i:s") . " -- zipping db WP backup...\n";
+		shell_exec("zip -9pr $backupname_db_wp db_wp_backup.sql");
+		echo date("h:i:s") . " -- WP Database dump complete!\n";
+	}
 }
 
 // Backup site files
@@ -88,6 +108,7 @@ if (file_exists("$backupname_site")) {
 // md5 for local backup
 $md5_1 = md5_file($backupname_site);
 $md5_2 = md5_file($backupname_db);
+$md5_3 = md5_file($backupname_db_wp);
 
 // Set API Timeout
 define('RAXSDK_TIMEOUT', '3600');
@@ -162,10 +183,26 @@ if ($md5_2 != $etag) {
 	echo date("h:i:s") . " -- $backupname_db moved to Cloud Files Successful!\n";
 }
 
+//WP DB BACKUP
+
+echo date("h:i:s") . " -- Moving $backupname_db_wp to Cloud Files...\n";
+$obj->Create(array('name' => $backupname_db_wp, 'content_type' => 'application/x-gzip'), $filename = $backupname_db_wp);
+
+// get etag(md5)
+$etag = $obj->hash;
+
+// compare md5 wih etag
+if ($md5_3 != $etag) {
+	$obj->Delete(array('name' => $backupname_db_wp));
+	echo date("h:i:s") . " -- Backup failed integrity check! Please try again.\n";
+} else {
+	echo date("h:i:s") . " -- $backupname_db_wp moved to Cloud Files Successful!\n";
+}
+
 if ($db_backup == "true") {
 	echo date("h:i:s") . " -- Cleaning up local backups...\n";
-//After your backup has been uploaded, remove the zip from the filesystem.
-	shell_exec("rm $backupname_site ; rm db_backup.sql; rm $backupname_db");
+	//After your backup has been uploaded, remove the zip from the filesystem.
+	shell_exec("rm $backupname_site ; rm db_backup.sql; db_wp_backup.sql; rm $backupname_db; rm $backupname_db_wp");
 	echo date("h:i:s") . " -- Local backups cleaned up!\n";
 } else {
 	shell_exec("rm $backupname_site");
--- backup_scripts/sample.cron-backup-config.php
+++ backup_scripts/sample.cron-backup-config.php
@@ -4,12 +4,16 @@
 ###############################################################
 # Developed by Jereme Hancock for Cloud Sites
 ###############################################################
-
 //Set information specific to your site
 $db_host = "";
 $db_user = "";
 $db_password = "";
 $db_name = "";
+//WP DB INFO
+$db_wp_host = "";
+$db_wp_user = "";
+$db_wp_password = "";
+$db_wp_name = "";
 //domain;
 $url = "www.openstack.org";
 //full server path to site
