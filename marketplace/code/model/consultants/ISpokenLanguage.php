2545b78a73edb619d8e249da3631538b5191151e
Sebastian Marcet <smarcet@gmail.com>
2014-11-20 15:44:55 -0300

    [smarcet] * ss3 migration fixes.

--- marketplace/code/model/consultants/ISpokenLanguage.php
+++ marketplace/code/model/consultants/ISpokenLanguage.php
@@ -1,4 +1,5 @@
-<?php/**
+<?php
+/**
  * Copyright 2014 Openstack.org
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
