47df285b011d64dec6d3cebc6dc86496b0bde68e
Sebastian Marcet <smarcet@gmail.com>
2014-12-16 11:18:50 -0300

    [smarcet] -  #7879 * added search box on view deployments * added remove feature on user stories

--- themes/openstack/javascript/sangria/view.deployment.details.js
+++ themes/openstack/javascript/sangria/view.deployment.details.js
@@ -1,3 +1,15 @@
+/**
+ * Copyright 2014 Openstack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 jQuery(document).ready(function($){
 
     $('.addDeploymentBtn').click(function(){
@@ -30,6 +42,7 @@ jQuery(document).ready(function($){
 
     var date_to   = $.QueryString["date-to"];
     var date_from = $.QueryString["date-from"];
+    var free_text = $.QueryString["free-text"];
 
     if(date_to!="undefined"){
         $("#date-to").val(date_to);
@@ -37,6 +50,10 @@ jQuery(document).ready(function($){
 
     if(date_from!="undefined"){
         $("#date-from").val(date_from);
+    }
+
+    if(free_text!="undefined"){
+        $("#free-text").val(free_text);
     }
 
     $.urlParam = function(name){
