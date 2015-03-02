b595b86de75f2bd9a9971974c3e3d52cf70d02fb
Santi Palenque <santipalenque@gmail.com>
2015-03-02 13:42:51 -0300

    [spalenque] - #8415 - body not showing fix

--- news/code/ui/frontend/js/news.form.js
+++ news/code/ui/frontend/js/news.form.js
@@ -106,7 +106,7 @@ jQuery(document).ready(function($){
 
             });
         },
-        forced_root_block : true,
+        forced_root_block : "p",
         height: "250px",
         width: "800px"
     });
