beec3e3570ace69244283f6831bed59dd416df9f
Sebastian Marcet <smarcet@gmail.com>
2015-04-02 13:47:32 -0300

    [smarcet] - #8607 * update task to use SS template engine

--- surveys/code/infrastructure/tasks/DeploymentSurveyTask.php
+++ surveys/code/infrastructure/tasks/DeploymentSurveyTask.php
@@ -11,81 +11,57 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
+
 /**
  * Class DeploymentSurveyTask
  */
-final class DeploymentSurveyTask extends CronTask {
+final class DeploymentSurveyTask extends CronTask
+{
 
-	function run(){
+    function run()
+    {
 
-		$batch_size = 15;
+        $batch_size = 15;
 
-		if(isset($_GET['batch_size'])){
-			$batch_size = intval(trim(Convert::raw2sql($_GET['batch_size'])));
-		}
+        if (isset($_GET['batch_size'])) {
+            $batch_size = intval(trim(Convert::raw2sql($_GET['batch_size'])));
+        }
 
-		$surveys     = DeploymentSurvey::getNotDigestSent($batch_size);
-		$deployments = Deployment::getNotDigestSent($batch_size);
+        $surveys     = DeploymentSurvey::getNotDigestSent($batch_size);
+        $deployments = Deployment::getNotDigestSent($batch_size);
 
-		$title1 = "User Surveys completed";
-		$title2 = "Deployment profiles completed";
 
-		$body = "";
+        if ($surveys) {
+            foreach ($surveys as $survey) {
+                $survey->SendDigest = 1;
+                $survey->write();
+            }
+        }
 
-		$body .= "<style>";
-		$body .= "*{ font-family: sans-serif;} p {font-size: 11px}";
-		$body .= "</style>";
+        if ($deployments) {
+            foreach ($deployments as $dep) {
+                $dep->SendDigest = 1;
+                $dep->write();
+            }
+        }
 
-		$body .= "<h2>" . $title1 . "</h2>";
+        if ((!is_null($surveys) && count($surveys)) > 0 || (!is_null($deployments) && count($deployments))) {
+         
+            global $email_new_deployment;
 
-		$send = false;
+            $email = EmailFactory::getInstance()->buildEmail($email_new_deployment, $email_new_deployment, $subject = 'New Deployments and Surveys');
 
-		if($surveys){
+            $email->setTemplate('NewDeploymentsSurveysEmail');
 
-			foreach($surveys as $survey){
-				$org_name  =  intval($survey->OrgID) > 0 ?  $survey->Org()->Name:$survey->Member()->getOrgName();
-				$body .= "<p><b>Org: " .$org_name  . "</b>";
-				$body .= "<br>Updated: " . $survey->UpdateDate;
-				$body .= '<br><a href="' . Director::absoluteURL('admin/deployments/DeploymentSurvey/' . $survey->ID .'/edit') . '" target="_blank">View Details</a>';
-				$body .= "</p>";
+            $email->populateTemplate(array(
+                'SurveysUrl'            => Director::absoluteURL('admin/deployments/DeploymentSurvey/EditForm/field/DeploymentSurvey/item'),
+                'DeploymentsUrl'        => Director::absoluteURL('admin/deployments/Deployment/EditForm/field/Deployment/item'),
+                'SangriaDeploymentsUrl' => Director::absoluteURL('sangria/ViewDeploymentDetails'),
+                'Surveys'               => $surveys,
+                'Deployments'           => $deployments
+            ));
 
-				$survey->SendDigest = 1;
-				$survey->write();
-			}
-
-			$send = true;
-
-		}
-
-		$body .= "<h2>" . $title2 . "</h2>";
-
-		if($deployments){
-
-			foreach($deployments as $dep){
-				$org_name  =  intval($dep->OrgID) > 0 ?  $dep->Org()->Name:$dep->DeploymentSurvey()->Member()->getOrgName();
-				$body .= "<p><b>Org: " . $org_name . "</b>";
-				$body .= "<br>Updated: " . $dep->UpdateDate;
-				$body .= "<br>Is Public: " . $dep->IsPublic;
-				if(!$dep->IsPublic){
-					$body .= '<br><a href="' . Director::absoluteURL('admin/deployments/Deployment/' . $dep->ID .'/edit') . '" target="_blank">View Details</a>';
-				}else{
-					$body .= '<br><a href="' . Director::absoluteURL('sangria/ViewDeploymentDetails?dep=' . $dep->ID ) . '" target="_blank">View Details</a>';
-				}
-				$body .= "</p>";
-
-				$dep->SendDigest = 1;
-				$dep->write();
-			}
-
-			$send = true;
-
-		}
-
-		if($send == true){
-			//echo $body;
-			global $email_new_deployment;
-			$email = EmailFactory::getInstance()->buildEmail($email_new_deployment, $email_new_deployment, 'New Deployments and Surveys', $body);
-			$email->send();
-		}
-	}
+            $email->send();
+        }
+    }
 } 
