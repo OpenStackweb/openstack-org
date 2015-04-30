b3d50eaf0ddbb6e42f27b0cd6821c41968caf2a9
Sebastian Marcet <smarcet@gmail.com>
2015-04-30 10:50:04 -0300

    [smarcet] - #8736 * tweak on tinymce configuration

--- openstack/_config.php
+++ openstack/_config.php
@@ -101,4 +101,6 @@ Object::add_extension('SangriaPage_Controller', 'SangriaPageViewSpeakingSubmissi
 Object::add_extension('SangriaPage_Controller', 'SangriaSetCategorySponsorsExtension');
 Object::add_extension('SangriaPage_Controller', 'SangriaPageInvolvementTypeExtension');
 
-HTTP::set_cache_age(0);
+/* TinyMCE Configuration */
+$tinyMCE = HtmlEditorConfig::get('cms');
+$tinyMCE->setOption('extended_valid_elements', '@[id|class|style|title],#a[id|rel|rev|dir|tabindex|accesskey|type|name|href|target|title|class],-strong/-b[class],-em/-i[class],-strike[class],-u[class],#p[id|dir|class|align|style],-ol[class],-ul[class],-li[class],br,i,em,img[id|dir|longdesc|usemap|class|src|border|alt=|title|width|height|align],-sub[class],-sup[class],-blockquote[dir|class],-table[border=0|cellspacing|cellpadding|width|height|class|align|summary|dir|id|style],-tr[id|dir|class|rowspan|width|height|align|valign|bgcolor|background|bordercolor|style],tbody[id|class|style],thead[id|class|style],tfoot[id|class|style],#td[id|dir|class|colspan|rowspan|width|height|align|valign|scope|style],-th[id|dir|class|colspan|rowspan|width|height|align|valign|scope|style],caption[id|dir|class],-div[id|dir|class|align|style],-span[class|align|style],-pre[class|align|name],address[class|align],-h1[id|dir|class|align|style],-h2[id|dir|class|align|style],-h3[id|dir|class|align|style],-h4[id|dir|class|align|style],-h5[id|dir|class|align|style],-h6[id|dir|class|align|style],hr[class],dd[id|class|title|dir],dl[id|class|title|dir],dt[id|class|title|dir],@[id,style,class]');
