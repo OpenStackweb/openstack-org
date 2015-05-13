73472501cf3565b874a43ac0477ef086a9da1bca
Sebastian Marcet <smarcet@gmail.com>
2015-05-13 16:26:25 -0300

    [smarcet] - #8806 * Added drag & reorder

--- summit/code/models/SummitUpdate.php
+++ summit/code/models/SummitUpdate.php
@@ -1,13 +1,17 @@
 <?php
 
+/**
+ * Class SummitUpdate
+ */
 
 class SummitUpdate extends DataObject
 {
 
     private static $db = array (
-        'Title' => 'Text',
-        'Category' => "Enum('News,Speakers,Sponsors,Attendees')",
-        'Description' => 'HTMLText'
+        'Title'       => 'Text',
+        'Category'    => "Enum('News,Speakers,Sponsors,Attendees')",
+        'Description' => 'HTMLText',
+        'Order'       => 'Int',
     );
 
     private static $has_one = array (
@@ -19,6 +23,13 @@ class SummitUpdate extends DataObject
         'Created',
         'Title',
         'Category'
-    );    
+    );
 
+    public function getCMSFields() {
+        $fields = new FieldList();
+        $fields->add(new TextField('Title','Title'));
+        $fields->add(new HtmlEditorField('Description','Description'));
+        $fields->add(new DropdownField('Category', 'Category', $this->dbObject('Category')->enumValues()));
+        return $fields;
+    }
 }
