aa27d7e1abf9638c4e84a989269fd04846333e01
Todd Morey <toddmorey@Todds-MBP.(none)>
2015-05-14 14:27:06 -0500

    SS3 modification to remove SiteTree being called statically.

--- openstack/code/summit/FeaturedVideo.php
+++ openstack/code/summit/FeaturedVideo.php
@@ -14,38 +14,46 @@ class FeaturedVideo extends DataObject {
     'PresentationCategoryPage' => 'PresentationCategoryPage'
   );
 
-function onBeforeWrite() {
-  parent::onBeforeWrite();
+  private function generateURLSegment($title){
+    $filter = URLSegmentFilter::create();
+    $t = $filter->filter($title);
 
+    // Fallback to generic page name if path is empty (= no valid, convertable characters)
+    if(!$t || $t == '-' || $t == '-1') $t = "page-$this->ID";
 
-  // If there is no URLSegment set, generate one from Title
-  if((!$this->URLSegment || $this->URLSegment == 'new-presentation') && $this->Title != 'New Presentation')
+    return $t;
+  } 
+
+  function onBeforeWrite()
   {
-    $this->URLSegment = SiteTree::generateURLSegment($this->Title);
-  }
-  else if($this->isChanged('URLSegment'))
-  {
-    // Make sure the URLSegment is valid for use in a URL
-    $segment = preg_replace('/[^A-Za-z0-9]+/','-',$this->URLSegment);
-    $segment = preg_replace('/-+/','-',$segment);
+    parent::onBeforeWrite();
 
-    // If after sanitising there is no URLSegment, give it a reasonable default
-    if(!$segment) {
-      $segment = "presentation-".$this->ID;
+
+    // If there is no URLSegment set, generate one from Title
+    if ((!$this->URLSegment || $this->URLSegment == 'new-presentation') && $this->Title != 'New Presentation') {
+      $this->URLSegment = $this->generateURLSegment($this->Title);
+    } else if ($this->isChanged('URLSegment')) {
+      // Make sure the URLSegment is valid for use in a URL
+      $segment = preg_replace('/[^A-Za-z0-9]+/', '-', $this->URLSegment);
+      $segment = preg_replace('/-+/', '-', $segment);
+
+      // If after sanitising there is no URLSegment, give it a reasonable default
+      if (!$segment) {
+        $segment = "presentation-" . $this->ID;
+      }
+      $this->URLSegment = $segment;
     }
-    $this->URLSegment = $segment;
+
+    // Ensure that this object has a non-conflicting URLSegment value by appending number if needed
+    $count = 2;
+    while ($this->LookForExistingURLSegment($this->URLSegment)) {
+      $this->URLSegment = preg_replace('/-[0-9]+$/', null, $this->URLSegment) . '-' . $count;
+      $count++;
+    }
+
   }
 
-  // Ensure that this object has a non-conflicting URLSegment value by appending number if needed
-  $count = 2;
-  while($this->LookForExistingURLSegment($this->URLSegment))
-  {
-    $this->URLSegment = preg_replace('/-[0-9]+$/', null, $this->URLSegment) . '-' . $count;
-    $count++;
-  }
-
-}
-
+  
 //Test whether the URLSegment exists already on another Video
 function LookForExistingURLSegment($URLSegment)
 {
