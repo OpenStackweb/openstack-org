211ca696628343b54f2b2e3608c1c08a4525bd5d
Wes Wilson <wes@iamweswilson.com>
2015-05-14 12:29:33 -0500

    converting image urls to production DB

--- openstack/code/landing-pages/EnterpriseWorkloadPage.php
+++ openstack/code/landing-pages/EnterpriseWorkloadPage.php
@@ -26,10 +26,6 @@ class EnterpriseWorkloadPage_Controller extends Page_Controller {
 
         Requirements::javascript('themes/openstack/javascript/filetracking.jquery.js');
         Requirements::javascript('themes/openstack/javascript/enterprise.js');   
-<<<<<<< HEAD
-=======
-        Requirements::javascript('themes/openstack/javascript/bootstrap.lightbox.js');        
->>>>>>> 9154668... Digital Workflow landing page added - need to fix lightbox
 
         Requirements::customScript("jQuery(document).ready(function($) {
 
