d2428a80e8a4afbdf54bb06bb20f5cef4d39e424
Sebastian Marcet <smarcet@gmail.com>
2015-06-01 13:31:46 -0300

    [smarcet] - #8895 * WIP - Making Summit pages editable through CMS

--- summit/code/pages/SummitOverviewPage.php
+++ summit/code/pages/SummitOverviewPage.php
@@ -2,7 +2,172 @@
 
 class SummitOverviewPage extends SummitPage {
 
-    
+    private static $has_one = array(
+        'GrowthBoxBackground' => 'BetterImage',
+        'GrowthBoxChartLegend' => 'File',
+        'GrowthBoxChart'       => 'File',
+        'GrowthBoxChartLegendPng' => 'File',
+        'GrowthBoxChartPng'       => 'File',
+    );
+
+    private static $db = array(
+        'GrowthBoxTextTop'    => 'HTMLText',
+        'GrowthBoxTextBottom' => 'HTMLText',
+        'VideoRecapCaption'   => 'Text',
+        'VideoRecapYouTubeID' => 'Text',
+        'ScheduleTitle' => 'Text',
+        'ScheduleText' => 'HTMLText',
+        'ScheduleUrl' => 'Text',
+        'NetworkingContent' => 'HTMLText',
+    );
+
+    private static $has_many = array(
+        'NetworkingPhotos' => 'SummitNetworkingPhoto',
+    );
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+
+
+        if($this->ID > 0){
+            $fields->addFieldToTab('Root.Networking', new HtmlEditorField('NetworkingContent', 'Content'));
+            //networking photos
+            $config = GridFieldConfig_RecordEditor::create();
+            $config->addComponent(new GridFieldSortableRows('Order'));
+            $gridField = new GridField('NetworkingPhotos', 'Photos', $this->NetworkingPhotos(), $config);
+            $fields->addFieldToTab('Root.Networking', $gridField);
+        }
+
+        $fields->addFieldsToTab('Root.Schedule', new TextField('ScheduleTitle','Title'));
+        $fields->addFieldsToTab('Root.Schedule', new HtmlEditorField('ScheduleText','Text'));
+        $fields->addFieldsToTab('Root.Schedule', new TextField('ScheduleUrl','Url'));
+
+        // GrowthBox
+        $fields->addFieldsToTab('Root.VideoRecap', new TextField('VideoRecapCaption','Caption Text'));
+        $fields->addFieldsToTab('Root.VideoRecap', new TextField('VideoRecapYouTubeID','YouTubeID'));
+
+        // GrowthBox
+        $fields->addFieldToTab("Root.GrowthBox", new HtmlEditorField('GrowthBoxTextTop','Text Top'));
+        $fields->addFieldToTab("Root.GrowthBox", new HtmlEditorField('GrowthBoxTextBottom','Text Bottom'));
+        $fields->addFieldsToTab("Root.GrowthBox", $upload_0 = new UploadField('GrowthBoxBackground','Background Image'));
+        $fields->addFieldsToTab("Root.GrowthBox", $upload_1 = new UploadField('GrowthBoxChartLegend','Chart Legend (SVG)'));
+        $fields->addFieldsToTab("Root.GrowthBox", $upload_2 = new UploadField('GrowthBoxChartLegendPng','Chart Legend (PNG)'));
+        $fields->addFieldsToTab("Root.GrowthBox", $upload_3 = new UploadField('GrowthBoxChart','Chart (SVG)'));
+        $fields->addFieldsToTab("Root.GrowthBox", $upload_4 = new UploadField('GrowthBoxChartPng','Chart (PNG)'));
+
+
+        $upload_0->setFolderName('summits/overview');
+        $upload_0->setAllowedMaxFileNumber(1);
+        $upload_0->setAllowedFileCategories('image');
+
+        $upload_1->setFolderName('summits/overview');
+        $upload_1->setAllowedMaxFileNumber(1);
+        $upload_1->setAllowedExtensions(array('svg'));
+
+        $upload_2->setFolderName('summits/overview');
+        $upload_2->setAllowedMaxFileNumber(1);
+        $upload_3->setAllowedExtensions(array('png'));
+
+        $upload_3->setFolderName('summits/overview');
+        $upload_3->setAllowedMaxFileNumber(1);
+        $upload_3->setAllowedExtensions(array('svg'));
+
+        $upload_4->setFolderName('summits/overview');
+        $upload_4->setAllowedMaxFileNumber(1);
+        $upload_4->setAllowedExtensions(array('png'));
+
+        return $fields;
+    }
+
+    public function getGrowthBoxTextTop(){
+        if(empty($this->getField('GrowthBoxTextTop')))
+            return '<h2>Join The Movement</h2><p>In 2010, 75 people met in Austin, Texas for the very first OpenStack Summit. Four years later, almost 5,000 attendees joined us in Paris for our second international summit and the first OpenStack Summit held in Europe.</p>';
+        return $this->getField('GrowthBoxTextTop');
+    }
+
+    public function getGrowthBoxTextBottom(){
+        if(!empty($this->getField('GrowthBoxTextBottom')))
+            return ' <p>The OpenStack summit is a unique opportunity for the developers and users of OpenStack software to meet and exchange ideas. Hundreds of the core developers will be on site to discuss all things OpenStack. Summits include in-depth technical discussions, hands-on workshops, and the full presence of almost every player in the OpenStack Ecosystem. If you are deploying OpenStack—or considering how it can help your enterprise—there’s no better way to connect with the community than the OpenStack Summit.</p>';
+        return $this->getField('GrowthBoxTextBottom');
+    }
+
+    public function BoxChartLegendImageUrl(){
+        if($this->GrowthBoxChartLegend()->exists()){
+            return $this->GrowthBoxChartLegend()->getURL();
+        }
+        return '/summit/images//line-growth-legend.svg';
+    }
+
+    public function BoxChartLegendImagePngUrl(){
+        if($this->GrowthBoxChartLegendPng()->exists()){
+            return $this->GrowthBoxChartLegendPng()->getURL();
+        }
+        return '/summit/images//line-growth-legend.png';
+    }
+
+    public function BoxChartImageUrl(){
+        if($this->GrowthBoxChart()->exists()){
+            return $this->GrowthBoxChart()->getURL();
+        }
+        return '/summit/images/line-growth-chart.svg';
+    }
+
+    public function BoxChartImagePngUrl(){
+        if($this->GrowthBoxChartPng()->exists()){
+            return $this->GrowthBoxChartPng()->getURL();
+        }
+        return '/summit/images/line-growth-chart.png';
+    }
+
+    public function BoxChartBackgroundImageUrl(){
+        if($this->GrowthBoxBackground()->exists()){
+            return $this->GrowthBoxBackground()->getURL();
+        }
+        return '/summit/images/growth-bkgd.jpg';
+    }
+
+    public function getVideoRecapCaption(){
+
+        if(empty($this->getField('VideoRecapCaption')))
+            return 'Video: See a recap of our May 2015 summit in Vancouver, BC.';
+        return $this->getField('VideoRecapCaption');
+    }
+
+    public function getVideoRecapYouTubeID(){
+        if(empty($this->getField('VideoRecapYouTubeID')))
+            return 'iZdEwQ-76P4';
+        return $this->getField('VideoRecapYouTubeID');
+    }
+
+    public function getScheduleText(){
+        if(empty($this->getField('ScheduleText')))
+        return '<p>Get a glimpse into the wealth of speakers, topics and sessions happening at OpenStack Summit Vancouver.</p>';
+        return $this->getField('ScheduleText');
+    }
+
+    public function getScheduleUrl(){
+        if(empty($this->getField('ScheduleUrl')))
+        return '/summit/vancouver-2015/schedule/';
+        return $this->getField('ScheduleUrl');
+    }
+
+    public function getScheduleTitle(){
+        if(empty($this->getField('ScheduleTitle')))
+        return 'Schedule';
+        return $this->getField('ScheduleTitle');
+    }
+
+    public function getNetworkingContent(){
+        if(empty($this->getField('NetworkingContent')))
+        return '<h1>Knowledge, Nightlife, &amp; New Friends.</h1>
+                <p>
+                    The Summit is about more than great sessions and speakers. Join the OpenStack community with
+                    networking events and fantastic nightlife.
+                </p>';
+        return $this->getField('NetworkingContent');
+    }
 }
 
 
@@ -11,6 +176,6 @@ class SummitOverviewPage_Controller extends SummitPage_Controller {
 	public function init() {
         $this->top_section = 'full';
         parent::init();
-	}    
-    
+	}
+
 }
--- summit/javascript/host-city.js
+++ summit/javascript/host-city.js
@@ -1,4 +1,53 @@
 // City nav active on scroll
+
+// City Nav Affix
+var num = 1070; //number of pixels before modifying styles
+
+// Setup the different icons and shadows
+var iconURLPrefix = 'http://openstack.org/summit/images/mapicons/';
+
+var icons = [
+    iconURLPrefix + 'venue.png',
+    iconURLPrefix + 'airport.png',
+    iconURLPrefix + '1.png',
+    iconURLPrefix + '2.png',
+    iconURLPrefix + '3.png',
+    iconURLPrefix + '4.png',
+    iconURLPrefix + '5.png',
+    iconURLPrefix + '6.png',
+    iconURLPrefix + '7.png',
+    iconURLPrefix + '8.png',
+    iconURLPrefix + '9.png',
+    iconURLPrefix + '10.png',
+    iconURLPrefix + '11.png',
+    iconURLPrefix + '12.png',
+    iconURLPrefix + '13.png',
+    iconURLPrefix + '14.png',
+    iconURLPrefix + '15.png',
+    iconURLPrefix + '16.png',
+    iconURLPrefix + '17.png',
+    iconURLPrefix + '18.png',
+    iconURLPrefix + '19.png',
+    iconURLPrefix + '20.png'
+]
+var icons_length = icons.length;
+
+
+var shadow = {
+    anchor: new google.maps.Point(15,33),
+    url: iconURLPrefix + 'shadow50.png'
+};
+
+var map = null;
+
+var infowindow = new google.maps.InfoWindow({
+    maxWidth: 400
+});
+
+var marker;
+var markers = new Array();
+
+var iconCounter = 0;
 $(document).ready(function () {
     $(document).on("scroll", onScroll);
     
@@ -31,6 +80,19 @@ $(document).ready(function () {
             $(document).on("scroll", onScroll);
         });
     });
+
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+        zoom: 16,
+        scrollwheel: false,
+        center: new google.maps.LatLng(settings.host_city_lat, settings.host_city_lng),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        streetViewControl: false,
+        panControl: false,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_BOTTOM
+        }
+    });
 });
 
 function onScroll(event){
@@ -48,8 +110,7 @@ function onScroll(event){
     });
 }
 
-// City Nav Affix
-var num = 1070; //number of pixels before modifying styles
+
 
 $(window).bind('scroll', function () {
     if ($(window).scrollTop() > num) {
@@ -58,63 +119,7 @@ $(window).bind('scroll', function () {
         $('.city-nav.city').removeClass('fixed');
     }
 });
-    
-    // Setup the different icons and shadows
-    var iconURLPrefix = 'http://openstack.org/summit/images/mapicons/';
-    
-    var icons = [
-      iconURLPrefix + 'venue.png',
-      iconURLPrefix + 'airport.png',
-      iconURLPrefix + '1.png',
-      iconURLPrefix + '2.png',
-      iconURLPrefix + '3.png',
-      iconURLPrefix + '4.png',
-      iconURLPrefix + '5.png',
-      iconURLPrefix + '6.png',
-      iconURLPrefix + '7.png',
-      iconURLPrefix + '8.png',
-      iconURLPrefix + '9.png',
-      iconURLPrefix + '10.png',
-      iconURLPrefix + '11.png',
-      iconURLPrefix + '12.png',
-      iconURLPrefix + '13.png',
-      iconURLPrefix + '14.png',
-      iconURLPrefix + '15.png',
-      iconURLPrefix + '16.png',
-      iconURLPrefix + '17.png',
-      iconURLPrefix + '18.png',
-      iconURLPrefix + '19.png',
-      iconURLPrefix + '20.png'
-    ]
-    var icons_length = icons.length;
-    
-    
-    var shadow = {
-      anchor: new google.maps.Point(15,33),
-      url: iconURLPrefix + 'shadow50.png'
-    };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-      zoom: 16,
-      scrollwheel: false,
-      center: new google.maps.LatLng(49.287141, -123.116976),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeControl: false,
-      streetViewControl: false,
-      panControl: false,
-      zoomControlOptions: {
-         position: google.maps.ControlPosition.LEFT_BOTTOM
-      }
-    });
-
-    var infowindow = new google.maps.InfoWindow({
-      maxWidth: 400
-    });
-
-    var marker;
-    var markers = new Array();
-    
-    var iconCounter = 0;
     
     function AutoCenter() {
       //  Create a new viewpoint bound
