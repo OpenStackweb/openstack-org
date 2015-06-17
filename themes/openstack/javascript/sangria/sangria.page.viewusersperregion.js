5f6e5f21b24a98e36b895aa4f4f289b6cdc2d678
spalenque <santipalenque@gmail.com>
2015-06-17 20:19:39 +0200

    [spalenque] - #8908 - export func for users per region

--- themes/openstack/javascript/sangria/sangria.page.viewusersperregion.js
+++ themes/openstack/javascript/sangria/sangria.page.viewusersperregion.js
@@ -34,4 +34,53 @@ jQuery(document).ready(function($){
         }
     });
 
+    $('.export_cb_all').click(function(){
+        var cb_value = $(this).prop('checked');
+        $('.export_cb').prop('checked',cb_value);
+    });
+
+    $('.export_cb_cont').click(function(){
+        var cb_value = $(this).prop('checked');
+        $('.export_cb_all').prop('checked',false);
+        $('.export_cb',$(this).parent()).prop('checked',cb_value);
+    });
+
+    var form_export_members = $('#form-export-members');
+
+    var form_validator = form_export_members.validate({
+        onfocusout: false,
+        rules: {
+            'countries[]'  : {required: true, minlength: 1 },
+            'members[]' : {required: true, minlength: 1}
+        },
+        messages: {
+            'countries[]': "Select a country.",
+            'members[]': "Select a member type."
+        },
+        focusInvalid: false,
+        invalidHandler: function(form, validator) {
+            if (!validator.numberOfInvalids())
+                return;
+            var element = $(validator.errorList[0].element);
+            if(!element.is(":visible")){
+                element = element.parent();
+            }
+        },
+        errorPlacement: function(error, element) {
+            $('.error').html(error.text()).show();
+
+            //$('#submit_export').after('<span for="countries[]" class="error">'+error.text()+'</span>');
+            //error.insertAfter($('#submit_export'));
+        }
+    });
+
+    form_export_members.submit(function(event){
+        var is_valid = form_export_members.valid();
+        if(!is_valid){
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+        }
+    })
+
 });
