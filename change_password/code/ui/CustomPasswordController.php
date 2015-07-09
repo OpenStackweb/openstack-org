ad1d69888de44e726275ea5859d37131ab3f4a36
Sebastian Marcet <smarcet@gmail.com>
2015-07-09 01:13:51 -0300

    [smarcet] - #9116 * fixed leaked password token reset

--- change_password/code/ui/CustomPasswordController.php
+++ change_password/code/ui/CustomPasswordController.php
@@ -11,86 +11,106 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
+
 /***
  * Class CustomPasswordController
  */
-class CustomPasswordController extends Security {
+class CustomPasswordController extends Security
+{
 
-	private static $allowed_actions = array(
-		'changepassword',
-		'ChangePasswordForm',
-	);
+    private static $allowed_actions = array(
+        'changepassword',
+        'ChangePasswordForm',
+    );
 
-	/**
-	 * @var PasswordManager
-	 */
-	private $password_manager;
+    /**
+     * @var PasswordManager
+     */
+    private $password_manager;
 
-	public function __construct(){
-		parent::__construct();
-		$this->password_manager = new PasswordManager;
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->password_manager = new PasswordManager;
+    }
 
-	/**
-	 * Factory method for the lost password form
-	 *
-	 * @return Form Returns the lost password form
-	 */
-	public function ChangePasswordForm() {
-		return new CustomChangePasswordForm($this, 'ChangePasswordForm');
-	}
+    /**
+     * Factory method for the lost password form
+     * @return Form Returns the lost password form
+     */
+    public function ChangePasswordForm()
+    {
+        return new CustomChangePasswordForm($this, 'ChangePasswordForm');
+    }
 
-	/**
-	 * @return string
-	 */
-	public function changepassword() {
-		$tmpPage             = new Page();
-		$tmpPage->Title      = _t('Security.CHANGEPASSWORDHEADER', 'Change your password');
-		$tmpPage->URLSegment = 'Security';
-		$tmpPage->ID         = -1; // Set the page ID to -1 so we dont get the top level pages as its children
-		$controller          = new Page_Controller($tmpPage);
-		$controller->init();
+    /**
+     * @return string
+     */
+    public function changepassword()
+    {
+        $tmpPage = new Page();
+        $tmpPage->Title = _t('Security.CHANGEPASSWORDHEADER', 'Change your password');
+        $tmpPage->URLSegment = 'Security';
+        $tmpPage->ID = -1; // Set the page ID to -1 so we dont get the top level pages as its children
+        $controller = new Page_Controller($tmpPage);
+        $controller->init();
 
-		try{
-			$former_hash = Session::get('AutoLoginHash');
-			if(!empty($former_hash)){
-				// Subsequent request after the "first load with hash"
-				$customisedController = $controller->customise(array(
-					'Content' =>
-						'<p>' .
-						_t('Security.ENTERNEWPASSWORD', 'Please enter a new password.') .
-						'</p>',
-					'Form' => $this->ChangePasswordForm(),
-				));
-			}
-			else if(isset($_REQUEST['t']) && isset($_REQUEST['m'])){
-				$new_hash  = $this->password_manager->verifyToken((int)@$_REQUEST['m'], @$_REQUEST['t']);
-				Session::set('AutoLoginHash', $new_hash);
-				return $this->redirect($this->Link('changepassword'));
-			}
-			else if(Member::currentUser()) {
-				// Logged in user requested a password change form.
-				$customisedController = $controller->customise(array(
-					'Content' => '<p>'
-						. _t('Security.CHANGEPASSWORDBELOW', 'You can change your password below.') . '</p>',
-					'Form' => $this->ChangePasswordForm()));
-			}
-			else{
-				self::permissionFailure(
-					$this,
-					_t('Security.ERRORPASSWORDPERMISSION', 'You must be logged in in order to change your password!')
-				);
-				return;
-			}
-		}
-		catch(InvalidPasswordResetLinkException $ex1){
-			$customisedController = $controller->customise(
-				array('Content' =>
-					sprintf('<p>This link is no longer valid as a newer request for a password reset has been made. Please check your mailbox for the most recent link</p><p>You can request a new one <a href="%s">here',
-						$this->Link('lostpassword'))
-				)
-			);
-		}
-		return $customisedController->renderWith(array('Security_changepassword', 'Security', $this->stat('template_main'), 'ContentController'));
-	}
+        try {
+            $former_hash = Session::get('AutoLoginHash');
+
+            // if we have the token and the member redirect back to clear those values and avoid leaking
+            // on referer header
+            if (isset($_REQUEST['t']) && isset($_REQUEST['m'])) {
+                // if we dont have a former autologin hash, generate it ...
+                if (empty($former_hash)) {
+                    $new_hash = $this->password_manager->verifyToken((int)@$_REQUEST['m'], @$_REQUEST['t']);
+                    Session::set('AutoLoginHash', $new_hash);
+                }
+                return $this->redirect($this->Link('changepassword'));
+            }
+
+            if (!empty($former_hash)) {
+                // Subsequent request after the "first load with hash"
+                $customisedController = $controller->customise(array(
+                    'Content' =>
+                        '<p>' .
+                        _t('Security.ENTERNEWPASSWORD', 'Please enter a new password.') .
+                        '</p>',
+                    'Form' => $this->ChangePasswordForm(),
+                ));
+            } else {
+                if (Member::currentUser()) {
+                    // Logged in user requested a password change form.
+                    $customisedController = $controller->customise(array(
+                        'Content' => '<p>'
+                            . _t('Security.CHANGEPASSWORDBELOW', 'You can change your password below.') . '</p>',
+                        'Form' => $this->ChangePasswordForm()
+                    ));
+                } else {
+                    self::permissionFailure(
+                        $this,
+                        _t('Security.ERRORPASSWORDPERMISSION',
+                            'You must be logged in in order to change your password!')
+                    );
+
+                    return;
+                }
+            }
+        } catch (InvalidPasswordResetLinkException $ex1) {
+            $customisedController = $controller->customise(
+                array(
+                    'Content' =>
+                        sprintf('<p>This link is no longer valid as a newer request for a password reset has been made. Please check your mailbox for the most recent link</p><p>You can request a new one <a href="%s">here',
+                            $this->Link('lostpassword'))
+                )
+            );
+        }
+
+        return $customisedController->renderWith(array(
+            'Security_changepassword',
+            'Security',
+            $this->stat('template_main'),
+            'ContentController'
+        ));
+    }
 }
