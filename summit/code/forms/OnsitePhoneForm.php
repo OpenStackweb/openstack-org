2f88c6c10d33e1efaf2b37f69084134d2c3c483b
Todd Morey <toddmorey@Todds-MBP.(none)>
2015-08-26 10:38:52 -0500

    Modifying to extend BootStrapForm (for proper formatting)

--- summit/code/forms/OnsitePhoneForm.php
+++ summit/code/forms/OnsitePhoneForm.php
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-class OnsitePhoneForm extends Form {
+class OnsitePhoneForm extends BootstrapForm {
  
    function __construct($controller, $name, $speakerHash) {
    
