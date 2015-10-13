c0a914a62148a7c797764435257d73c4e373283d
Sebastian Marcet <smarcet@gmail.com>
2015-10-13 14:14:23 -0300

    [smarcet] - #9657 * fix on marketplace admin / version filtering

--- marketplace/code/ui/admin/js/openstack.release.supported.api.version.admin.ui.js
+++ marketplace/code/ui/admin/js/openstack.release.supported.api.version.admin.ui.js
@@ -2,19 +2,23 @@
 
     $.entwine('ss.custom', function ($) {
 
-        $(".ddl-api-version-id").entwine({
+        $("div.ddl-api-version-id").entwine({
             initialize: function()
             {
 
             },
             onmatch: function ()
             {
+                console.log('version admin onmatch');
+
                 var ddl_versions  = $('select','.ddl-api-version-id');
                 var ddl_component = $('select','.ddl-os-component-id');
 
                 ddl_component.change(function(event){
                     var component_id = $(this).val();
                     var component_versions = versions[component_id];
+
+                    console.log('on select api version');
                     ddl_versions.empty(); //remove all child nodes
                     ddl_versions.html('');
                     ddl_versions.append("<option value='' selected='selected'>-- Please Select --</option>");
