8ae418770b2cec4fc91629df05ca46d68be380d5
Sebastian Marcet <smarcet@gmail.com>
2015-10-14 10:02:33 -0300

    [smarcet] - #9669 * fixed webpack configuration to make more generic
    Conflicts:
    	summit/javascript/new-trackchairs-app/public/bundle.js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+     /* here u must define all your entries points*/
+    "software/js/public/software_all_projects": "./software/js/software_all_projects",
+    "summit/javascript/new-trackchairs-app/app/index": "./summit/javascript/new-trackchairs-app/app/index",
+  },
+  output: {
+    path: __dirname ,
+    filename: "[name].bundle.js",
+    chunkFilename: "[id].bundle.js"
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      riot: 'riot'
+    }),
+    new webpack.optimize.UglifyJsPlugin(
+          {
+              sourceMap: false,
+              mangle:    false,
+              minimize:  true
+          }
+    )
+  ],
+  module: {
+    loaders: [
+      { test: /\.tag$/, loader: 'tag' },
+    ]
+  }
+};
