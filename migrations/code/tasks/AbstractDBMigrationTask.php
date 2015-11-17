4c49fa0c31017f6a05836d00ea465677d498dd4c
Sebastian Marcet <smarcet@gmail.com>
2015-11-17 11:55:07 -0300

    [smarcet] * Added  MigrationDB tasks and migration module

---
+++ migrations/code/tasks/AbstractDBMigrationTask.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Copyright 2015 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+class AbstractDBMigrationTask extends MigrationTask
+{
+    protected $title;
+
+    protected $description;
+
+    function up()
+    {
+        echo sprintf("starting migration # %s ...", $this->title).PHP_EOL;
+        //check if migration already had ran ...
+        $migration = Migration::get()->filter('Name', $this->title)->first();
+
+        if (!$migration) {
+
+            $this->doUp();
+            $migration = new Migration();
+            $migration->Name = $this->title;
+            $migration->Description = $this->description;
+            $migration->Write();
+            echo sprintf("ending migration # %s ...", $this->title).PHP_EOL;
+        }
+        else
+        {
+            echo sprintf("migration # %s already ran !...", $this->title).PHP_EOL;
+        }
+    }
+
+    function down()
+    {
+        $this->doDown();
+    }
+
+    public function doUp(){}
+
+    public function doDown(){}
+
+}
---
+++ migrations/code/tasks/DBMigrateTask.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Copyright 2015 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+final class DBMigrateTask extends MigrationTask
+{
+    protected $title = "DB migration task";
+
+    protected $description = "DB migration task";
+
+    private static $migrations = array
+    (
+        
+    );
+
+    function up()
+    {
+        echo "Starting DB Migration Proc ...".PHP_EOL;
+        if(count(self::$migrations))
+        {
+            echo sprintf("found %s migrations to run ...", count(self::$migrations)).PHP_EOL;
+
+            foreach(self::$migrations as $migration_class){
+                $m = new $migration_class;
+                if($m instanceof MigrationTask)
+                {
+                    echo "running migration ".$migration_class.PHP_EOL;
+                    $m->up();
+                }
+            }
+        }
+        echo "Ending DB Migration Proc ...".PHP_EOL;
+    }
+
+}
