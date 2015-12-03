0d24bbe9f00fb2b5cccc95a32d8b3a211e210981
spalenque <santipalenque@gmail.com>
2015-12-03 13:16:43 -0300

    [spalenque] - plural url for event details

--- summit/code/pages/SummitAppSchedPage.php
+++ summit/code/pages/SummitAppSchedPage.php
@@ -17,7 +17,7 @@ class SummitAppSchedPage_Controller extends SummitPage_Controller {
     );
 
     static $url_handlers = array(
-        'event/$EVENT_ID/$EVENT_TITLE' => 'ViewEvent',
+        'events/$EVENT_ID/$EVENT_TITLE' => 'ViewEvent',
         'speakers/$SPEAKER_ID'          => 'ViewSpeakerProfile',
         'attendees/$ATTENDEE_ID'        => 'ViewAttendeeProfile',
         'global-search'                 => 'DoGlobalSearch',
