3f586558b7f4ada019312b865bf46257868c8d5e
Sebastian Marcet <smarcet@gmail.com>
2016-02-02 17:43:45 -0300

    [smarcet] - #10345 * set page size to higher value

--- marketplace/code/infrastructure/active_records/interop/InteropProgramVersion.php
+++ marketplace/code/infrastructure/active_records/interop/InteropProgramVersion.php
@@ -36,7 +36,7 @@ class InteropProgramVersion extends DataObject implements IInteropProgramVersion
         $fields =  new FieldList();
         $fields->add(new TextField('Name','Name'));
         if($this->ID){
-            $config = GridFieldConfig_RelationEditor::create();
+            $config = GridFieldConfig_RelationEditor::create(100);
             $config->addComponent(new GridFieldSortableRows('Order'));
 
             $data_columns = $config->getComponentByType('GridFieldDataColumns');
@@ -49,7 +49,7 @@ class InteropProgramVersion extends DataObject implements IInteropProgramVersion
             $gridField = new GridField('Capabilities', 'Capabilities', $this->Capabilities(), $config);
             $fields->add($gridField);
 
-            $config = GridFieldConfig_RelationEditor::create();
+            $config = GridFieldConfig_RelationEditor::create(100);
             $config->addComponent(new GridFieldSortableRows('Order'));
 
             $data_columns = $config->getComponentByType('GridFieldDataColumns');
