3476b53b182db944c21e337e81f25e2179dda96c
Sebastian Marcet <smarcet@gmail.com>
2016-02-29 23:36:47 -0300

    [smarcet] - #10534 * fix on date null condtion

--- marketplace/code/model/training/TrainingCourseLocationDTO.php
+++ marketplace/code/model/training/TrainingCourseLocationDTO.php
@@ -11,105 +11,115 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
+
 /**
  * Class TrainingCourseLocationDTO
  */
-class TrainingCourseLocationDTO {
-	/**
-	 * @var int
-	 */
-	private $id;
-	/**
-	 * @var string
-	 */
-	private $city;
-	/**
-	 * @var string
-	 */
-	private $state;
-	/**
-	 * @var string
-	 */
-	private $country;
-	/**
-	 * @var string
-	 */
-	private $start_date;
-	/**
-	 * @var string
-	 */
-	private $end_date;
-	/**
-	 * @var string
-	 */
-	private $link;
+class TrainingCourseLocationDTO
+{
+    /**
+     * @var int
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $city;
+    /**
+     * @var string
+     */
+    private $state;
+    /**
+     * @var string
+     */
+    private $country;
+    /**
+     * @var string
+     */
+    private $start_date;
+    /**
+     * @var string
+     */
+    private $end_date;
+    /**
+     * @var string
+     */
+    private $link;
 
-	/**
-	 * @param int $id
-	 * @param string $city
-	 * @param string $state
-	 * @param string $country
-	 * @param string $start_date
-	 * @param string $end_date
-	 * @param string $link
-	 */
-	public function __construct($id, $city, $state, $country, $start_date, $end_date, $link){
-		$this->id         = $id;
-		$this->city       = $city;
-		$this->state      = $state;
-		$this->country    = $country;
-		$this->start_date = $start_date;
-		$this->end_date   = $end_date;
-		$this->link       = $link;
-	}
+    /**
+     * @param int $id
+     * @param string $city
+     * @param string $state
+     * @param string $country
+     * @param string $start_date
+     * @param string $end_date
+     * @param string $link
+     */
+    public function __construct($id, $city, $state, $country, $start_date, $end_date, $link)
+    {
+        $this->id         = $id;
+        $this->city       = $city;
+        $this->state      = $state;
+        $this->country    = $country;
+        $this->start_date = $start_date;
+        $this->end_date   = $end_date;
+        $this->link       = $link;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getIdentifier(){
-		return $this->id;
-	}
+    /**
+     * @return int
+     */
+    public function getIdentifier()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getCity(){
-		return $this->city;
-	}
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getState(){
-		return $this->state;
-	}
+    /**
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getCountry(){
-		return $this->country;
-	}
+    /**
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getStartDate(){
-		return $this->start_date;
-	}
+    /**
+     * @return string
+     */
+    public function getStartDate()
+    {
+        return $this->start_date;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getEndDate(){
-		return $this->end_date;
-	}
+    /**
+     * @return string
+     */
+    public function getEndDate()
+    {
+        return $this->end_date;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getLink(){
-		return $this->link;
-	}
+    /**
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
 
 } 
--- marketplace/code/ui/frontend/TrainingDirectoryPage.php
+++ marketplace/code/ui/frontend/TrainingDirectoryPage.php
@@ -11,6 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
+
 /**
  * Class TrainingDirectoryPage
  */
@@ -22,95 +23,104 @@ class TrainingDirectoryPage extends MarketPlaceDirectoryPage
 /**
  * Class TrainingDirectoryPage_Controller
  */
-class TrainingDirectoryPage_Controller extends MarketPlaceDirectoryPage_Controller {
+class TrainingDirectoryPage_Controller extends MarketPlaceDirectoryPage_Controller
+{
 
-	private static $allowed_actions = array('classes','handleIndex');
+    private static $allowed_actions = array('classes', 'handleIndex');
 
-	static $url_handlers = array(
+    static $url_handlers = array(
         'classes' => 'classes',
         '$Company!/$Slug' => 'handleIndex'
-	);
+    );
 
-	/**
-	 * @var TrainingFacade $training_facade
-	 */
-	private $training_facade;
+    /**
+     * @var TrainingFacade $training_facade
+     */
+    private $training_facade;
 
-	/**
-	 * @var IQueryHandler
-	 */
-	private $course_location_query;
-	/**
-	 * @var IQueryHandler
-	 */
-	private $course_level_query;
+    /**
+     * @var IQueryHandler
+     */
+    private $course_location_query;
+    /**
+     * @var IQueryHandler
+     */
+    private $course_level_query;
     /**
      * @var IQueryHandler
      */
     private $course_company_query;
 
-	function init(){
+    function init()
+    {
         parent::init();
 
         Requirements::css("themes/openstack/css/chosen.css", "screen,projection");
         Requirements::css(THIRDPARTY_DIR . '/jquery-ui-themes/smoothness/jquery-ui.css');
-    	Requirements::javascript(THIRDPARTY_DIR . '/jquery-ui/jquery-ui.js');
+        Requirements::javascript(THIRDPARTY_DIR . '/jquery-ui/jquery-ui.js');
 
         Requirements::combine_files('marketplace_training_landing.js',
             array(
                 "themes/openstack/javascript/chosen.jquery.min.js",
                 "marketplace/code/ui/frontend/js/training.directory.page.js"
-        ));
+            ));
 
-		 Requirements::customScript("jQuery(document).ready(function($) {
+        Requirements::customScript("jQuery(document).ready(function($) {
             $('#training','.marketplace-nav').addClass('current');
         });");
 
-	    $this->training_facade = new TrainingFacade(
-		    $this,
-		    new TrainingManager(new SapphireTrainingServiceRepository,
-			new SapphireMarketPlaceTypeRepository,
-			new TrainingAddPolicy,
-			new TrainingShowPolicy,
-			new SessionCacheService,
-			new MarketplaceFactory,
-			SapphireTransactionManager::getInstance()),
-		    new SapphireCourseRepository(new MarketplaceFactory)
-	    );
+        $this->training_facade = new TrainingFacade(
+            $this,
+            new TrainingManager(new SapphireTrainingServiceRepository,
+                new SapphireMarketPlaceTypeRepository,
+                new TrainingAddPolicy,
+                new TrainingShowPolicy,
+                new SessionCacheService,
+                new MarketplaceFactory,
+                SapphireTransactionManager::getInstance()),
+            new SapphireCourseRepository(new MarketplaceFactory)
+        );
 
-		$this->course_location_query = new TrainingCoursesLocationQueryHandler;
-		$this->course_level_query    = new TrainingCoursesLevelQueryHandler;
-        $this->course_company_query    = new TrainingCoursesCompanyQueryHandler;
+        $this->course_location_query = new TrainingCoursesLocationQueryHandler;
+        $this->course_level_query = new TrainingCoursesLevelQueryHandler;
+        $this->course_company_query = new TrainingCoursesCompanyQueryHandler;
 
-	    Requirements::customScript($this->GATrackingCode());
+        Requirements::customScript($this->GATrackingCode());
     }
 
-	/**
-	 * @return ArrayList|TrainingViewModel
-	 */
-	public function getTrainings(){
-		return $this->training_facade->getTrainings();
-	}
+    /**
+     * @return ArrayList|TrainingViewModel
+     */
+    public function getTrainings()
+    {
+        return $this->training_facade->getTrainings();
+    }
 
-    public function handleIndex() {
+    public function handleIndex()
+    {
         $params = $this->request->allParams();
-        if(isset($params["Company"])){
+        if (isset($params["Company"])) {
             //render instance ...
             return $this->training();
         }
     }
 
-	public function training()
+    public function training()
     {
         try {
-	        Requirements::css("marketplace/code/ui/frontend/css/training.detail.css");
-	        $params              = $this->request->allParams();
-	        $company_url_segment = Convert::raw2sql($params["Company"]);
-	        $training_id         = Convert::raw2sql(@$params["Slug"]);
-	        $training            = $this->training_facade->getCompanyTraining($training_id,$company_url_segment);
+            Requirements::css("marketplace/code/ui/frontend/css/training.detail.css");
+            $params = $this->request->allParams();
+            $company_url_segment = Convert::raw2sql($params["Company"]);
+            $training_id = Convert::raw2sql(@$params["Slug"]);
+            $training = $this->training_facade->getCompanyTraining($training_id, $company_url_segment);
             // we need this for reviews.
             $this->company_service_ID = $training['Training']->getIdentifier();
-            return $this->Customise($training)->renderWith(array('TrainingDirectoryPage_training','TrainingDirectoryPage','MarketPlacePage'));
+
+            return $this->Customise($training)->renderWith(array(
+                'TrainingDirectoryPage_training',
+                'TrainingDirectoryPage',
+                'MarketPlacePage'
+            ));
 
         } catch (Exception $ex) {
             return $this->httpError(404, 'Sorry that Training could not be found!-');
@@ -120,50 +130,60 @@ class TrainingDirectoryPage_Controller extends MarketPlaceDirectoryPage_Controll
     /*
      * top 20 "upcoming courses" for all companies
      */
-    function getUpcomingCourses($limit = 20){
-	     return $this->training_facade->getUpcomingCourses($limit);
+    function getUpcomingCourses($limit = 20)
+    {
+        return $this->training_facade->getUpcomingCourses($limit);
     }
 
-    function getAllClasses(){
+    function getAllClasses()
+    {
         return $this->training_facade->getAllClasses();
     }
 
-	public function LocationCombo(){
-		$source = array();
-		$result = $this->course_location_query->handle(new OpenStackImplementationNamesQuerySpecification(DateTimeUtils::getCurrentDate()));
-		foreach($result->getResult() as $dto){
-			$source[$dto->getValue()] =  $dto->getValue();
-		}
-		$ddl = new DropdownField('location-term',$title=null,$source);
-		$ddl->setEmptyString('-- Show All --');
-		return $ddl;
-	}
-
-	public function LevelCombo(){
-		$source = array();
-		$result = $this->course_level_query->handle(new OpenStackImplementationNamesQuerySpecification(''));
-		foreach($result->getResult() as $dto){
-			$source[$dto->getValue()] =  $dto->getValue();
-		}
-		$ddl = new DropdownField('level-term',$title=null,$source);
-		$ddl->setEmptyString('-- Show All --');
-		return $ddl;
-	}
-
-    public function CompanyCombo(){
+    public function LocationCombo()
+    {
         $source = array();
-        $result = $this->course_company_query->handle(new OpenStackImplementationNamesQuerySpecification(DateTimeUtils::getCurrentDate()));
-        foreach($result->getResult() as $dto){
-            $source[$dto->getValue()] =  $dto->getValue();
+        $result = $this->course_location_query->handle(new OpenStackImplementationNamesQuerySpecification(DateTimeUtils::getCurrentDate()));
+        foreach ($result->getResult() as $dto) {
+            $source[$dto->getValue()] = $dto->getValue();
         }
-        $ddl = new DropdownField('company-term',$title=null,$source);
+        $ddl = new DropdownField('location-term', $title = null, $source);
         $ddl->setEmptyString('-- Show All --');
+
         return $ddl;
     }
 
-    public function classes() {
+    public function LevelCombo()
+    {
+        $source = array();
+        $result = $this->course_level_query->handle(new OpenStackImplementationNamesQuerySpecification(''));
+        foreach ($result->getResult() as $dto) {
+            $source[$dto->getValue()] = $dto->getValue();
+        }
+        $ddl = new DropdownField('level-term', $title = null, $source);
+        $ddl->setEmptyString('-- Show All --');
+
+        return $ddl;
+    }
+
+    public function CompanyCombo()
+    {
+        $source = array();
+        $result = $this->course_company_query->handle(new OpenStackImplementationNamesQuerySpecification(DateTimeUtils::getCurrentDate()));
+        foreach ($result->getResult() as $dto) {
+            $source[$dto->getValue()] = $dto->getValue();
+        }
+        $ddl = new DropdownField('company-term', $title = null, $source);
+        $ddl->setEmptyString('-- Show All --');
+
+        return $ddl;
+    }
+
+    public function classes()
+    {
         Requirements::Block("marketplace/code/ui/frontend/js/training.directory.page.js");
         Requirements::javascript("marketplace/code/ui/frontend/js/training.classes.page.js");
-        return $this->renderWith(array('TrainingDirectoryPage_classes','TrainingDirectoryPage','MarketPlacePage'));
+
+        return $this->renderWith(array('TrainingDirectoryPage_classes', 'TrainingDirectoryPage', 'MarketPlacePage'));
     }
 }
