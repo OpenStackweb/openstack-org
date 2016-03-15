03c6a7264d11fc03552bebc066654d8cdb760144
spalenque <santipalenque@gmail.com>
2016-03-15 13:46:57 -0300

    [spalenque] - #10389 - fix query

--- survey_builder/code/infrastructure/repositories/SapphireSurveyAnswerRepository.php
+++ survey_builder/code/infrastructure/repositories/SapphireSurveyAnswerRepository.php
@@ -30,11 +30,23 @@ class SapphireAnswerSurveyRepository
      */
     public function getByQuestionAndFilters($question_id, $filters = null)
     {
-        $answers_query = "SELECT qval.`Value` FROM SurveyAnswer AS a
-                        LEFT JOIN SurveyStep AS step ON step.ID = a.StepID
-                        LEFT JOIN Survey AS s ON s.ID = step.SurveyID
-                        LEFT JOIN SurveyQuestionValueTemplate AS qval ON a.`Value` = qval.ID
-                        WHERE a.QuestionID = {$question_id}";
+        $answers_query = "  SELECT qval.`Value` FROM SurveyAnswer AS a
+                            LEFT JOIN SurveyStep AS step ON step.ID = a.StepID";
+
+        if ($filters) {
+            $q = SurveyQuestionTemplate::get_by_id('SurveyQuestionTemplate',$question_id);
+            if ($q->Step()->SurveyTemplate()->ClassName == 'EntitySurveyTemplate') {
+                $answers_query .= " LEFT JOIN EntitySurvey AS es ON es.ID = step.SurveyID
+                                   LEFT JOIN Survey AS s ON s.ID = es.ParentID";
+            } else {
+                $answers_query .= " LEFT JOIN Survey AS s ON s.ID = step.SurveyID";
+            }
+        } else {
+            $answers_query .= " LEFT JOIN Survey AS s ON s.ID = step.SurveyID";
+        }
+
+        $answers_query .= " LEFT JOIN SurveyQuestionValueTemplate AS qval ON a.`Value` = qval.ID
+                            WHERE a.QuestionID = {$question_id}";
 
         if ($filters) {
             $filter_query = "SELECT DISTINCT(q0.ID) FROM ";
@@ -46,13 +58,13 @@ class SapphireAnswerSurveyRepository
                 $filter_query .= "(SELECT s.ID FROM Survey AS s ";
 
                 if ($filter_q->Step()->SurveyTemplate()->ClassName == 'EntitySurveyTemplate') {
-                    $filter_query .= "LEFT JOIN EntitySurvey AS es ON es.ParentID = s.ID
-                                          LEFT JOIN SurveyStep AS step ON step.SurveyID = es.ID ";
+                    $filter_query .= " LEFT JOIN EntitySurvey AS es ON es.ParentID = s.ID
+                                          LEFT JOIN SurveyStep AS step ON step.SurveyID = es.ID";
                 } else {
-                    $filter_query .= "LEFT JOIN SurveyStep AS step ON step.SurveyID = s.ID ";
+                    $filter_query .= " LEFT JOIN SurveyStep AS step ON step.SurveyID = s.ID";
                 }
 
-                $filter_query .= "LEFT JOIN SurveyAnswer AS a ON a.StepID = step.ID
+                $filter_query .= " LEFT JOIN SurveyAnswer AS a ON a.StepID = step.ID
                                       LEFT JOIN SurveyQuestionValueTemplate AS qval ON a.`Value` = qval.ID
                                       WHERE a.QuestionID = {$filter->id} AND qval.`Value` = '{$filter->value}' ) AS q".$key;
 
