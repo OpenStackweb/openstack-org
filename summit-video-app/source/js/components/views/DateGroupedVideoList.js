4be4c848427d6287b958ee684a5c8df8eaa2b295
Uncle Cheese <aaron@silverstripe.com>
2016-04-26 12:33:02 -0500

    BUGFIX: Incorrect dates on grouped video list

--- summit-video-app/source/js/components/views/DateGroupedVideoList.js
+++ summit-video-app/source/js/components/views/DateGroupedVideoList.js
@@ -7,24 +7,23 @@ export default ({
 	videos,
 	hasMore
 }) => {
-	const formatTitle = function (date, timezone = 'UTC') {	
-		let offset = new Date().getTimezoneOffset();		
+	const formatTitle = function (date) {	
 		let today = moment().format('YYYY-MM-DD');
-		if(moment(date).subtract(offset,'minutes').format('YYYY-MM-DD') === today) {
+		if(moment(date).format('YYYY-MM-DD') === today) {
 			return 'Uploaded today';
 		}
-		else if(moment(date).subtract(offset,'minutes').add(1, 'days').format('YYYY-MM-DD') === today) {
+		else if(moment(date).add(1, 'days').format('YYYY-MM-DD') === today) {
 			return 'Uploaded yesterday';
 		}
 
-		return 'Uploaded ' + moment(date).subtract(offset, 'minutes').format('MMMM D, YYYY');		
+		return 'Uploaded ' + moment(date).format('MMMM D, YYYY');		
 	}
 	const groupedVideos = groupedList(videos, 'date');
 
 	return (		
 		<div>
 			{groupedVideos.map((group,i) => (
-				<VideoPanel key={i} videos={group} title={formatTitle(group[0].dateUTC, group[0].timezone)} />				
+				<VideoPanel key={i} videos={group} title={formatTitle(group[0].date)} />				
 			))}
 		</div>
 	);
