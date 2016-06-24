bcedb5bb428722f86eaa77f9a78480f869c6765f
Sebastian Marcet <smarcet@gmail.com>
2016-06-24 05:46:13 -0300

    [smarcet] - #11295 * fixed migration logic, check for column existance

--- summit-trackchair-app/code/TrackChairMigration.php
+++ summit-trackchair-app/code/TrackChairMigration.php
@@ -5,11 +5,17 @@ class TrackChairMigration extends AbstractDBMigrationTask
 
 	public function doUp()
 	{
-		DB::query("UPDATE SummitCategoryChange SET Status = 1 WHERE Done=1");
-		DB::query("UPDATE SummitCategoryChange SET Status = 0 WHERE Done=0");
+        global $database;
 
-		DB::query("UPDATE SummitSelectedPresentation SET Collection = 'selected'");
+        if (DBSchema::existsColumn($database, 'SummitCategoryChange', 'Done')) {
+            DB::query("UPDATE SummitCategoryChange SET Status = 1 WHERE Done=1");
+            DB::query("UPDATE SummitCategoryChange SET Status = 0 WHERE Done=0");
+        }
 
-		DB::query("UPDATE SummitPresentationComment SET IsActivity = 1 WHERE IsCategoryChangeSuggestion = 1");
+        DB::query("UPDATE SummitSelectedPresentation SET Collection = 'selected'");
+
+        if (DBSchema::existsColumn($database, 'SummitPresentationComment', 'IsCategoryChangeSuggestion')) {
+            DB::query("UPDATE SummitPresentationComment SET IsActivity = 1 WHERE IsCategoryChangeSuggestion = 1");
+        }
 	}
 }
