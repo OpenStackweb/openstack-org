377f87fddba7f2288e34a572a5646da31e7400ea
Jimmy McArthur <jimmy@tipit.net>
2016-08-15 15:44:49 -0500

    Removed pitiful attempt at adding in login

--- survey_builder/code/ui/frontend/UserSurveyPage.php
+++ survey_builder/code/ui/frontend/UserSurveyPage.php
@@ -56,31 +56,6 @@ class UserSurveyPage extends SurveyPage
 		Foundation User Committee will be able to advocate on your behalf.</p>
 		<p><a href="{$link}faq" class="roundedButton">More Information About The Survey</a></p>
 		<hr/>
-		
-	    <h1>Get Started</h1>
-	    <div class="row">
-	        <div class="col-lg-6">
-	            <h3>Already have an OpenStack Foundation login?</h3>
-	            <div class="survey-login-wrapper">
-	                <form id="MemberLoginForm_LoginForm" action="Security/login?BackURL={$BackURL}" method="post"
-	                      enctype="application/x-www-form-urlencoded">
-	                    <input type="hidden" name="fragment" id="fragment"/>
-	                    <div class="Actions">
-	                        <input class="action " id="MemberLoginForm_LoginForm_action_dologin" type="submit"
-	                               name="action_dologin" value="Log in" title="Log in"/>
-	                        <p id="ForgotPassword"><a href="Security/lostpassword">I've lost my password</a></p>
-	                    </div>
-	                </form>
-	            </div>
-	        </div>
-	        <div class="col-lg-6">
-	            <h3>Don't have a login? Start here.</h3>
-	
-	            <div class="survey-login-wrapper">
-	                $RegisterForm
-	            </div>
-	        </div>
-	    </div>
 HTML;
         }
         return $res;
