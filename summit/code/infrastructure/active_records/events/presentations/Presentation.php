c945acb9a5ab64fed31215c7aacee3bf4b1e3e03
Jimmy McArthur <jimmy@tipit.net>
2016-08-25 17:12:05 -0500

    Removed ProblemAddressed validation

--- summit/code/infrastructure/active_records/events/presentations/Presentation.php
+++ summit/code/infrastructure/active_records/events/presentations/Presentation.php
@@ -713,14 +713,6 @@ SQL;
             throw new EntityValidationException('ShortDescription is mandatory!');
         }
 
-        if (empty($this->ProblemAddressed)) {
-            throw new EntityValidationException('ProblemAddressed is mandatory!');
-        }
-
-        if (empty($this->AttendeesExpectedLearnt)) {
-            throw new EntityValidationException('AttendeesExpectedLearnt is mandatory!');
-        }
-
         if (empty($this->Level)) {
             throw new EntityValidationException('Level is mandatory!');
         }
