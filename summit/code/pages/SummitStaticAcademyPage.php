f94fccf179727011ac38ca128d00053495db8dd6
Jimmy McArthur <jimmy@tipit.net>
2016-09-20 09:08:01 -0500

    Removed block of summit css

--- summit/code/pages/SummitStaticAcademyPage.php
+++ summit/code/pages/SummitStaticAcademyPage.php
@@ -14,7 +14,6 @@ class SummitStaticAcademyPage_Controller extends SummitPage_Controller {
     public function init()
     {
         parent::init();
-        Requirements::block("summit/css/combined.css");
         Requirements::css("themes/openstack/static/css/combined.css");
     }
 
