157ee18f6b1113c753f8ce5403946492112cb704
Jimmy McArthur <jimmy@tipit.net>
2016-09-20 08:59:05 -0500

    Removed Black of Summit CSS

--- summit/code/pages/SummitStaticBarcelonaGuidePage.php
+++ summit/code/pages/SummitStaticBarcelonaGuidePage.php
@@ -14,7 +14,6 @@ class SummitStaticBarcelonaGuidePage_Controller extends SummitPage_Controller {
     public function init()
     {
         parent::init();
-        Requirements::block("summit/css/combined.css");
         Requirements::css("themes/openstack/static/css/combined.css");
         Requirements::javascript("themes/openstack/static/js/guide.js");
     }
