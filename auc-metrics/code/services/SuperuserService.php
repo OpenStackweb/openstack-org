3d9060cf7791e22905313022a34aef2e292ceab9
Uncle Cheese <unclecheese@leftandmain.com>
2016-10-13 13:24:37 +1300

    New superuser metric

--- auc-metrics/code/services/SuperuserService.php
+++ auc-metrics/code/services/SuperuserService.php
@@ -2,6 +2,8 @@
 
 namespace OpenStack\AUC;
 
+use \Member;
+
 /**
  * Class SuperUserService
  * @package OpenStack\AUC
@@ -31,6 +33,32 @@ class SuperUserService extends BaseService implements MetricService
     public function run()
     {
         $this->results = ResultList::create();
+        $rss = new \SimplePie();
+        $rss->set_feed_url('http://superuser.openstack.org/feed');
+        $rss->init();
+
+        foreach($rss->get_items() as $item) {        	
+        	$author = $item->get_author();        	
+			$members = Member::get()->where("
+            	CONCAT_WS(' ', Member.FirstName, Member.Surname) LIKE '%{$author->name}%'
+			");
+			
+			$count = (int) $members->count();
+			if($count === 0) {
+				$this->logError("Could not find member $author");
+			}
+
+			else if($count > 1) {
+				$this->logError("Author $author matched multiple Member records (".implode(', ', $members->column('Email')).")");
+			}
+
+			else {
+				$this->results->push(Result::create(
+					$members->first(),
+					$item->get_title()
+				));
+			}
+        }
     }
 
 }
