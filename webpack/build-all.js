7c918f47e5c1ce74d64b1836d6f7b2cf9381a4e6
Uncle Cheese <unclecheese@leftandmain.com>
2016-11-22 21:54:01 +1300

    build all revisions

--- webpack/build-all.js
+++ webpack/build-all.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
 const fs = require('fs');
 
 const dir = path.join(__dirname, '../');
@@ -15,19 +16,22 @@ fs.readdir(dir, (err, list) => {
 		const dirPath = path.join(dir, entry);
 		if(fs.statSync(dirPath).isDirectory()) {
 			const configPath = path.join(dirPath, 'ui', 'webpack.config.js');
+			const packagePath = path.join(dirPath, 'ui', 'package.json');
 			try {
 				if(fs.statSync(configPath).isFile()) {
 					const moduleName = path.basename(dirPath);
-					console.log(`Building ${moduleName}`);
-					exec(`node ./webpack ${moduleName}`, (error, stdout, stderr) => {
-					  if (error) {
-					    console.error(`Error: ${error}`);
-					    return;
-					  }
-					  console.log(stdout);
-					  console.error(stderr);
-					});
-					
+					console.log(`
+
+/***************** 
+ * Building ${moduleName}
+\\*****************
+
+`);					if(fs.existsSync(packagePath)) {
+						console.log('Loading dependencies...');
+						execSync(`cd ${moduleName}/ui && npm install`, {stdio: 'inherit'});
+						execSync(`cd ${dir}`);
+					}	
+					execSync(`node ./webpack ${moduleName}`, {stdio: 'inherit'});
 				}
 			} catch (e) { }
 		}
