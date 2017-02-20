ab3352e3c0b68a865de380e9538b5bbe59a272ab
“Sebastian <smarcet@gmail.com>
2017-02-20 16:09:45 -0300

    [smarcet] * fixed merge conflict

--- summit-voting-app/ui/source/js/components/containers/PresentationList.js
+++ summit-voting-app/ui/source/js/components/containers/PresentationList.js
@@ -26,18 +26,11 @@ class PresentationList extends React.Component {
 	}
 
 	onPresentationClicked (id) {
-<<<<<<< HEAD
         this.updateLocationHash(id);
         this.props.dispatch(goToPresentation(id));
 		if(this.props.isMobile) {
 			this.props.dispatch(togglePresentationList());
 		}
-=======
-		// Hack!
-		document.body.classList.remove('openVotingNav');
-        this.updateLocationHash(id);
-        this.props.dispatch(goToPresentation(id));
->>>>>>> branch/project-nav-revamp
 	}
 
 	updateLocationHash(id){
