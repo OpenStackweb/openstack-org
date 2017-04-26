4668e219ad09cc29c589672a2c95f6730e4f29c7
spalenque <santipalenque@gmail.com>
2017-04-26 19:30:42 -0300

    [spalenque] - #12832 * add cron task to publish pages on publish date

--- openstack/code/extensions/FuturePublishDate.php
+++ openstack/code/extensions/FuturePublishDate.php
@@ -47,29 +47,3 @@ class FuturePublishDate extends DataExtension
     }
 
 }
-
-class FuturePublishDateController extends Extension
-{
-    public function beforeCallActionHandler($request, $action) {
-        if ('index' !== $action || $this->owner->is_a('ErrorPage_Controller')) {
-            return;
-        }
-
-        $isDraftPreview = 'Stage' === $request->getVar('stage');
-
-        if( !$isDraftPreview
-            && $this->owner->PublishDate
-            && strtotime($this->owner->PublishDate.' UTC') > time()
-        ){
-            // bug in SS 3.1 in OldPageRedirector
-            // $this->owner->httpError( 404 );
-
-            $response = $request->isMedia() ? null : ErrorPage::response_for(404);
-            if ($response) {
-                return $response;
-            }
-
-            throw new SS_HTTPResponse_Exception('404 Not Found', 404);
-        }
-    }
-}
