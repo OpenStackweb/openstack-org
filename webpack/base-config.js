675565b05065df839ad1fc7de39ef443615a9c5c
Ignacio Rodriguez Faillace <ignacio.faillace@gmail.com>
2017-07-14 16:31:33 -0300

    Upgraded webpack to version 2
    [smarcet]
    
    * updated dependencies
    * removed autoprefixer webpack plugin
    * added postcss-loader webpack plugin

--- webpack/base-config.js
+++ webpack/base-config.js
@@ -10,7 +10,7 @@ var EXCLUDE_JS = [/node_modules/];
  * @type {*[]}
  */
 var plugins = [
-    new ExtractTextPlugin('css/[name].css'),
+    new ExtractTextPlugin({ filename: 'css/[name].css' }),
     new webpack.ProvidePlugin({riot: 'riot'})
 ];
 
@@ -38,80 +38,87 @@ var productionPlugins = [
  * @param loader
  * @returns {*}
  */
-function styleLoader(loader) {
+function styleLoader(loaders) {
     if (PRODUCTION)
-        return ExtractTextPlugin.extract('style-loader', loader);
-    return 'style-loader!' + loader;
+        return ExtractTextPlugin.extract({ fallback: 'style-loader', use: loaders });
+    return [ 'style-loader', ...loaders ];
 }
 
 /**
  *
- * @returns {string}
+ * @returns {object}
  */
-function autoPrefixLoader() {
-    return 'autoprefixer-loader?' + JSON.stringify({
-        browsers: ['Firefox > 20', 'iOS 7', 'IE 9']
-    });
+function postCSSLoader() {
+    return {
+        loader: "postcss-loader",
+        options: {
+            plugins: function () {
+                return [require("autoprefixer")];
+            }
+        }
+    }
 }
 
 /**
  *
  * @type {*[]}
  */
-var loaders = [
+var rules = [
     {
         test: /\.js$/,
-        loaders: [
-            'react-hot', 'babel'
+        use: [
+            'react-hot-loader', 'babel-loader'
         ],
         exclude: EXCLUDE_JS,
         include: path.join(__dirname, '../')
     }, {
         test: /\.tag$/,
-        loader: 'babel!tag'
+        use: [
+            'babel-loader', 'tag-loader'
+        ]
     }, {
         test: /\.json$/,
-        loaders: ['json-loader']
+        use: ['json-loader']
     }, {
         test: /\.module\.css$/,
-        loader: styleLoader('css-loader?modules!' + autoPrefixLoader())
+        use: styleLoader(['css-loader?modules', postCSSLoader()])
     }, {
         test: /\.module\.less/,
-        loader: styleLoader('css-loader?modules!' + autoPrefixLoader() + '!less-loader')
+        use: styleLoader(['css-loader?modules', postCSSLoader(), 'less-loader'])
     }, {
         test: /\.module\.scss/,
-        loader: styleLoader('css-loader?modules!' + autoPrefixLoader() + '!sass-loader')
+        use: styleLoader(['css-loader?modules', postCSSLoader(), 'sass-loader'])
     }, {
         test: /\.css$/,
         exclude: /\.module\.css$/,
-        loader: styleLoader('css-loader!' + autoPrefixLoader())
+        use: styleLoader(['css-loader', postCSSLoader()])
     }, {
         test: /\.less/,
         exclude: /\.module\.less/,
-        loader: styleLoader('css-loader!' + autoPrefixLoader() + '!less-loader')
+        use: styleLoader(['css-loader', postCSSLoader(), 'less-loader'])
     }, {
         test: /\.scss/,
         exclude: /\.module\.scss/,
-        loader: styleLoader('css-loader!' + autoPrefixLoader() + '!sass-loader')
+        use: styleLoader(['css-loader', postCSSLoader(), 'sass-loader'])
     }, {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&minetype=application/font-woff&name=fonts/[name].[ext]"
+        use: "url-loader?limit=10000&minetype=application/font-woff&name=fonts/[name].[ext]"
     }, {
         test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader?name=fonts/[name].[ext]"
+        use: "file-loader?name=fonts/[name].[ext]"
     }, {
         test: /\.jpg|\.png|\.gif$/,
-        loader: "file-loader?name=images/[name].[ext]"
+        use: "file-loader?name=images/[name].[ext]"
     }, {
         test: /\.svg/,
-        loader: "file-loader?name=svg/[name].[ext]!svgo-loader"
+        use: "file-loader?name=svg/[name].[ext]!svgo-loader"
     }
 ];
 
 module.exports = {
 
     module: {
-        loaders
+        rules
     },
     plugins: PRODUCTION
         ? plugins.concat(productionPlugins)
