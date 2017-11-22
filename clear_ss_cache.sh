b5d542d0553a7a8cd4858f21d4652f033938f426
Sebastian Marcet <smarcet@gmail.com>
2017-11-22 16:50:48 -0300

    [smarcet] * updated clear ss cache command

--- clear_ss_cache.sh
+++ clear_ss_cache.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env bash
-sudo rm -R silverstripe-cache/*
+#!/bin/bash
+
+if [ ! -d silverstripe-cache ]; then
+	echo "creating silverstripe-cache directory";
+	mkdir -p silverstripe-cache;
+fi
+
+sudo rm -Rf silverstripe-cache/*
 sudo chmod 777 silverstripe-cache;
+sudo chown $USER:www-data silverstripe-cache;
