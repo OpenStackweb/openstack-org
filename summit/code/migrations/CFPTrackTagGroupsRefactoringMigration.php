653b3a55e99baf6d37eb84b6f7de7ada98a76bac
Sebastian Marcet <smarcet@gmail.com>
2018-07-11 17:44:09 -0300

    [smarcet] * fixed CFP track tag groups migration

--- summit/code/migrations/CFPTrackTagGroupsRefactoringMigration.php
+++ summit/code/migrations/CFPTrackTagGroupsRefactoringMigration.php
@@ -23,20 +23,23 @@ final class CFPTrackTagGroupsRefactoringMigration extends AbstractDBMigrationTas
 
     function doUp()
     {
+       global $database;
+
        $queries = [
-           "INSERT INTO DefaultTrackTagGroup
+
+           DBSchema::existsTable($database, 'TagGroup') ?  "INSERT INTO DefaultTrackTagGroup
 (ClassName, Created, LastEdited, `Name`, Label, `Order`, Mandatory)
 SELECT 'DefaultTrackTagGroup', NOW(), NOW(), TRIM(`Name`), TRIM(`Label`), `Order`, Mandatory FROM 
-TagGroup;",
-           "INSERT INTO DefaultTrackTagGroup_AllowedTags
+TagGroup;" : "",
+           DBSchema::existsTable($database, 'Summit_CategoryDefaultTags') ?  "INSERT INTO DefaultTrackTagGroup_AllowedTags
 (DefaultTrackTagGroupID, TagID)
 SELECT DefaultTrackTagGroup.ID, Tag.ID
 FROM Summit_CategoryDefaultTags 
 INNER JOIN DefaultTrackTagGroup ON DefaultTrackTagGroup.`Name` = Summit_CategoryDefaultTags.`Group`
 INNER JOIN Tag ON Tag.ID = Summit_CategoryDefaultTags.TagID
-WHERE SummitID = 24;",
-           "DROP TABLE Summit_CategoryDefaultTags;",
-           "DROP TABLE TagGroup;",
+WHERE SummitID = 24;" : "",
+           "DROP TABLE IF EXISTS `Summit_CategoryDefaultTags`;",
+           "DROP TABLE IF EXISTS `TagGroup`;",
            "INSERT INTO TrackTagGroup
 (Created, LastEdited, `Name`, `Label`, `Order`, `Mandatory`, SummitID)
 SELECT NOW(), NOW(), `Name`, `Label`, `Order`, `Mandatory`, 24 FROM DefaultTrackTagGroup;",
@@ -47,11 +50,12 @@ FROM DefaultTrackTagGroup_AllowedTags
 INNER JOIN DefaultTrackTagGroup ON DefaultTrackTagGroup.ID = DefaultTrackTagGroup_AllowedTags.DefaultTrackTagGroupID
 INNER JOIN Tag ON Tag.ID = DefaultTrackTagGroup_AllowedTags.TagID
 INNER JOIN TrackTagGroup ON DefaultTrackTagGroup.`Label` = TrackTagGroup.`Label`;",
-           "ALTER TABLE PresentationCategory_AllowedTags DROP COLUMN `Group`;",
-           "ALTER TABLE PresentationCategory_AllowedTags DROP COLUMN `IsDefault`;"
+           DBSchema::existsColumn($database, 'PresentationCategory_AllowedTags', `Group`) ? "ALTER TABLE PresentationCategory_AllowedTags DROP COLUMN `Group`;" : "",
+           DBSchema::existsColumn($database, 'PresentationCategory_AllowedTags', `IsDefault`) ? "ALTER TABLE PresentationCategory_AllowedTags DROP COLUMN `IsDefault`;": ""
        ];
 
        foreach($queries as $sql){
+           if(empty($sql)) continue;
            DB::query($sql);
        }
     }
