cd8ac6f8680516b666e5c2f7635f4dd106f84bd5
Santi <santipalenque@gmail.com>
2018-09-18 17:06:51 -0300

    [spalenque] - #14346 * add tracking and order complete popup to summit static page

--- summit/code/pages/StaticSummitAboutPage.php
+++ summit/code/pages/StaticSummitAboutPage.php
@@ -27,4 +27,89 @@ class StaticSummitAboutPage_Controller extends Page_Controller {
     public function getAboutPageNavClass(){
         return 'current';
     }
+
+    public function getOrder(){
+        $order = $this->request->getVar('order');
+        return isset($order) && $order == "complete";
+    }
+
+    /*
+    * Return google tracking script if ?order=complete query string param is present
+    *  using settings of current conference page
+    */
+    function GATrackingCode()
+    {
+        $request = $this->request;
+        $order = $request->requestVar("order");
+        $tracking_code = '';
+        if (isset($order) && $order == "complete") {
+            //add GA tracking script
+            $page = SummitPage::get()->filter('SummitID', 25)->first();
+            if ($page && !empty($page->GAConversionId)
+                && !empty($page->GAConversionLanguage)
+                && !empty($page->GAConversionFormat)
+                && !empty($page->GAConversionColor)
+                && !empty($page->GAConversionLabel)
+            ) {
+                $tracking_code = $this->renderWith("SummitPage_GA", array(
+                    "GA_Data" => new ArrayData(array(
+                        "GAConversionId" => $page->GAConversionId,
+                        "GAConversionLanguage" => $page->GAConversionLanguage,
+                        "GAConversionFormat" => $page->GAConversionFormat,
+                        "GAConversionColor" => $page->GAConversionColor,
+                        "GAConversionLabel" => $page->GAConversionLabel,
+                        "GAConversionValue" => $page->GAConversionValue,
+                        "GARemarketingOnly" => $page->GARemarketingOnly ? "true" : "false",
+                    ))
+                ));
+            }
+        }
+        return $tracking_code;
+    }
+
+    function FBTrackingCode()
+    {
+        $request       = $this->request;
+        $order         = $request->requestVar("order");
+        $tracking_code = '';
+
+        if (isset($order) && $order == "complete") {
+            //add FB tracking script
+            $page = SummitPage::get()->filter('SummitID', 25)->first();
+            if ($page && !empty($page->FBPixelId)) {
+                $tracking_code = $this->renderWith("SummitPage_FBPixelCode",[
+                    "FB_Data" => new ArrayData([
+                        "FBPixelId" => $page->FBPixelId,
+                    ])
+                ]);
+            }
+        }
+        return $tracking_code;
+    }
+
+    function TwitterTrackingCode()
+    {
+        $request = $this->request;
+        $order = $request->requestVar("order");
+        $tracking_code = '';
+        if (isset($order) && $order == "complete") {
+            //add FB tracking script
+            $page = SummitPage::get()->filter('SummitID', 25)->first();
+            if ($page && !empty($page->TwitterPixelId)
+            ) {
+                $tracking_code = $this->renderWith("SummitPage_Twitter", array(
+                    "Twitter_Data" => new ArrayData(array(
+                        "TwitterPixelId" => $page->TwitterPixelId,
+                    ))
+                ));
+            }
+        }
+        return $tracking_code;
+    }
+
+    public function MetaTags()
+    {
+        $tags = parent::MetaTags();
+        return $tags;
+    }
 }
