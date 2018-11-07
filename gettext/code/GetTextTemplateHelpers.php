10d0a2d35da88f8abb7fb361a4ff1dd75811fe5f
Sebastian Marcet <smarcet@gmail.com>
2018-11-07 20:13:37 -0300

    [smarcet] - #14443 * fixed issue with % and sprintf function on php

--- gettext/code/GetTextTemplateHelpers.php
+++ gettext/code/GetTextTemplateHelpers.php
@@ -112,7 +112,8 @@ class GetTextTemplateHelpers implements TemplateGlobalProvider
 
         if($decode_html)
             $msgid = html_entity_decode($msgid);
-
+        // scape % bc its has special meaning on sprintf
+        $msgid  = str_replace("%", "%%", $msgid);
         $msgstr = call_user_func_array("__", array_merge([$msgid], $args));
 
         return $msgstr;
