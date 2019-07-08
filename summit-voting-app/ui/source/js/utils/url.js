610cfb66c713a43141850150007400780e436b48
Santi <santipalenque@gmail.com>
2019-07-08 17:55:46 -0300

    [spalenque] - fix bug on voting app not working when testing on stage version

--- summit-voting-app/ui/source/js/utils/url.js
+++ summit-voting-app/ui/source/js/utils/url.js
@@ -9,10 +9,32 @@ const serialise = (obj) => {
   return str.join("&");
 };
 
+const getQueryStringParams = query => {
+    return query
+        ? (/^[?#]/.test(query) ? query.slice(1) : query)
+            .split('&')
+            .reduce((params, param) => {
+                    let [key, value] = param.split('=');
+                    params[key] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
+                    return params;
+                }, {}
+            )
+        : {}
+};
+
 export default (pathParts, queryParams, windowObj) => {
 	windowObj = windowObj || window;
 	let baseURL = Config.get('baseURL');
 	let path;
+
+	// remove query string from baseURL
+	let qsPos = baseURL.indexOf("?");
+	if (qsPos !== -1) {
+        let qsString = baseURL.slice(qsPos);
+        let qsObj = getQueryStringParams(qsString);
+        baseURL = baseURL.slice(0, qsPos);
+        queryParams = {...queryParams, ...qsObj};
+	}
 
 	// array
 	if(Array.isArray(pathParts)) {
