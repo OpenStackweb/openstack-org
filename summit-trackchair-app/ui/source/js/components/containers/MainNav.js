d4461e772681f8bda234683a709c30f51b7da658
Santi <santipalenque@gmail.com>
2019-09-10 13:49:50 -0300

    remove duplication in track chairs nav bar

--- summit-trackchair-app/ui/source/js/components/containers/MainNav.js
+++ summit-trackchair-app/ui/source/js/components/containers/MainNav.js
@@ -27,10 +27,6 @@ const MainNav = ({
         	<i className="fa fa-edit"></i>
         	<span className="nav-label">Change Requests</span>
         </LinkButton>
-		<LinkButton link="change-requests">
-			<i className="fa fa-edit"></i>
-			<span className="nav-label">Change Requests</span>
-		</LinkButton>
 		<li>
 			<a href="/Security/logout?BackURL=/Security/login">
 				<i className="fa fa-sign-out"></i>
