89100f748dba0fc2b33cb10cf3fb6e16ac64fdd8
smarcet <smarcet@gmail.com>
2019-11-21 00:35:38 -0300

    [smarcet] * fix for election purge

--- elections/code/infrastructure/factories/RevocationNotificationFactory.php
+++ elections/code/infrastructure/factories/RevocationNotificationFactory.php
@@ -17,15 +17,15 @@
 final class RevocationNotificationFactory implements IFoundationMemberRevocationNotificationFactory {
 
 	/**
-	 * @param IFoundationMember $foundation_member
+	 * @param Member $foundation_member
 	 * @param IElection         $last_election
 	 * @return IFoundationMemberRevocationNotification
 	 */
-	public function build(IFoundationMember $foundation_member, IElection $last_election)
+	public function build(Member $foundation_member, IElection $last_election)
 	{
 		$notification                  = new FoundationMemberRevocationNotification;
-		$notification->RecipientID     = $foundation_member->getIdentifier();
-		$notification->LastElectionID  = $last_election->getIdentifier();
+		$notification->RecipientID     = $foundation_member->ID;
+		$notification->LastElectionID  = $last_election->ID;
 		return $notification;
 	}
 }
