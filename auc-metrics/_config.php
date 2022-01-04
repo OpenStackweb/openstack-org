db3394a26922fffc6e93ade728418672e1d1acdc
Santi <santipalenque@gmail.com>
2022-01-04 14:55:45 -0300

    Update meetup API (#211)
    * WIP
    
    * meetup api using graphql

--- auc-metrics/_config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('AUC_METRICS_DIR', basename(__DIR__));
-require_once(BASE_PATH . '/' . AUC_METRICS_DIR . '/code/traits/ParserCreator.php');
-require_once(BASE_PATH . '/' . AUC_METRICS_DIR . '/code/traits/ProcessCreator.php');
--- auc-metrics/ui/webpack.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  entry: {
-    "missmatch-users" : "./source/js/missmatch_users/index.js",
-    "metrics-list"    : "./source/js/metrics_list/index.js",
-   }
-};
