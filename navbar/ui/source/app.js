49835a1ba2e7f247146b7b2e2d53443729cba3c6
smarcet <smarcet@gmail.com>
2022-07-14 14:04:35 -0300

    nav bar implementation
    Signed-off-by: smarcet <smarcet@gmail.com>

---
+++ navbar/ui/source/app.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import NavigationWidget from 'navigation-widget/dist';
+import 'navigation-widget/dist/index.css';
+//import ProjectsData from "./data.json";
+import {connect} from "react-redux";
+import {
+    fetchAll,
+
+} from "./actions";
+
+class App extends React.Component {
+
+    componentDidMount() {
+        let {projects, fetchAll} = this.props;
+        if(!projects.length) {
+            fetchAll();
+        }
+    }
+
+    render(){
+
+        let {projects} = this.props;
+
+        if(!projects.length) return null;
+
+        const widgetProps = {
+            projects: projects,
+            currentProject: window.navBarConfig.currentProject,
+        };
+
+        return (
+            <NavigationWidget {...widgetProps} />
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    ...state
+})
+
+export default connect (
+    mapStateToProps,
+    {
+        fetchAll,
+    }
+)(App);
+
