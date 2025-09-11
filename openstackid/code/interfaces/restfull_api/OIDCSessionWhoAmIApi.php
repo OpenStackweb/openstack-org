25c91e189d15ce8b39fd9e00c17fda1b88954d24
Matias Perrone <matias.perrone@exomindset.co>
2025-09-11 13:55:18 -0300

    Add oidc/session/whoami endpoint

---
+++ openstackid/code/interfaces/restfull_api/OIDCSessionWhoAmIApi.php
@@ -0,0 +1,155 @@
+<?php
+
+
+/**
+ * Copyright 2025 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+/**
+ * Class OIDCSessionWhoAmIApi
+ * Handles OIDC session whoami requests
+ */
+final class OIDCSessionWhoAmIApi extends AbstractRestfulJsonApi
+{
+	private static $api_prefix = 'oidc/session/whoami';
+
+	/**
+	 * @var array
+	 */
+	private static $url_handlers = [
+		'POST ' => 'index',
+	];
+
+	/**
+	 * @var array
+	 */
+	private static $allowed_actions = [
+		'index',
+	];
+
+	/**
+	 * @return bool
+	 */
+	protected function isApiCall()
+	{
+		$request = $this->getRequest();
+		if (is_null($request))
+			return false;
+		return true;
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function authenticate()
+	{
+		return true;
+	}
+
+	/**
+	 * Bootstrap OIDC session
+	 * @param SS_HTTPRequest $request
+	 * @return SS_HTTPResponse
+	 */
+	public function index(SS_HTTPRequest $request)
+	{
+		try {
+			$response = $this->validateRequestData($request);
+			if ($response instanceof SS_HTTPResponse) {
+				return $response;
+			}
+
+      $member = Member::currentUser();
+
+      $response = new SS_HTTPResponse();
+      $response->addHeader('Content-Type', 'application/json');
+
+      if (!$member) {
+        $response->setStatusCode(404);
+        $response->setBody(json_encode(['message' => 'No authenticated user found']));
+      }
+      else
+      {
+        $response->setStatusCode(200);
+        $response->setBody(json_encode([
+          'user_id' => $member->ID,
+          'email' => $member->Email,
+        ]));
+      }
+      return $response;
+
+		} catch (EntityValidationException $ex1) {
+			SS_Log::log($ex1, SS_Log::WARN);
+			return $this->validationError($ex1->getMessages());
+		} catch (Exception $ex) {
+			SS_Log::log($ex, SS_Log::ERR);
+			return $this->serverError();
+		}
+	}
+
+	/**
+	 * Validate request data
+	 * @param SS_HTTPRequest $request
+	 * @return bool|SS_HTTPResponse
+	 */
+	private function validateRequestData(SS_HTTPRequest $request)
+	{
+		// Check if request method is POST
+		if (!$request->isPOST()) {
+			return $this->methodNotAllowed();
+		}
+
+		// Check Content-Type header
+		$contentType = $request->getHeader('Content-Type');
+		if (strpos($contentType, 'application/json') === false) {
+			return $this->validationError(['Content-Type must be application/json']);
+		}
+
+		// Check X-CSRF-Token header
+		$csrfToken = $request->getHeader('X-CSRF-Token') ?: $request->getHeader('X-Csrf-Token');
+		if (empty($csrfToken)) {
+			return $this->validationError(['X-CSRF-Token header is required', "headers" => $request->getHeaders()]);
+		}
+
+		// Check X-CSRF-Token header value
+		if ($csrfToken !== $this->getSecurityToken()) {
+			return $this->badRequest('X-CSRF-Token header is invalid');
+		}
+
+		return true;
+	}
+
+	/**
+	 * Return method not allowed response
+	 * @param string $message
+	 * @return SS_HTTPResponse
+	 */
+	protected function methodNotAllowed()
+	{
+		return parent::methodNotAllowed()
+			->setBody(json_encode("Only POST requests are allowed"))
+			->addHeader('Allow', 'POST');
+	}
+
+	protected function getSecurityToken()
+	{
+		return SecurityToken::inst() ? SecurityToken::inst()->getValue() : null;
+	}
+}
